dd5b9ed26aad78b1bb1fcd3e595d18518933d65d
de4dot <de4dot@gmail.com>
2012-08-26 16:30:16 +0200

    Add project files

---
+++ dot10/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dot10")]
+[assembly: AssemblyDescription(".NET assembly reader/writer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dot10")]
+[assembly: AssemblyCopyright("Copyright (C) 2012 de4dot@gmail.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
