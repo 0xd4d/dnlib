671f47d81f277d6a0c51ca663c3773af20077f8e
de4dot <de4dot@gmail.com>
2012-08-28 19:47:57 +0200

    Add ExtensionAttribute

---
+++ dot10/ExtensionAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+#pragma warning disable 1591	// XML doc warning
+
+namespace System.Runtime.CompilerServices {
+	[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
+	public class ExtensionAttribute : Attribute {
+	}
+}
