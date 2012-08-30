a6dd8c2f8e1ed1ba304dd8fd3d5d341ca1a9ccd1
de4dot <de4dot@gmail.com>
2012-08-30 17:11:18 +0200

    Create IBinaryReader iface and have IImageStream implement it

---
+++ dot10/IO/IBinaryReader.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace dot10.IO {
+	/// <summary>
+	/// Reads binary data
+	/// </summary>
+	public interface IBinaryReader : IDisposable {
+		/// <summary>
+		/// Returns the length of the stream
+		/// </summary>
+		long Length { get; }
+
+		/// <summary>
+		/// Gets/sets the position
+		/// </summary>
+		long Position { get; set; }
+
+		/// <summary>
+		/// Reads <paramref name="size"/> bytes from the current <see cref="Position"/>
+		/// and increments <see cref="Position"/> by <paramref name="size"/> bytes
+		/// </summary>
+		/// <param name="size">Number of bytes to read</param>
+		/// <returns>All bytes</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		byte[] ReadBytes(int size);
+
+		/// <summary>
+		/// Reads a <see cref="byte"/> from the current position and increments <see cref="Position"/> by 1
+		/// </summary>
+		/// <returns>The 8-bit unsigned byte</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		byte ReadByte();
+
+		/// <summary>
+		/// Reads a <see cref="Int16"/> from the current position and increments <see cref="Position"/> by 2
+		/// </summary>
+		/// <returns>The 16-bit signed integer</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		short ReadInt16();
+
+		/// <summary>
+		/// Reads a <see cref="Int32"/> from the current position and increments <see cref="Position"/> by 4
+		/// </summary>
+		/// <returns>The 32-bit signed integer</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		int ReadInt32();
+
+		/// <summary>
+		/// Reads a <see cref="Int64"/> from the current position and increments <see cref="Position"/> by 8
+		/// </summary>
+		/// <returns>The 64-bit signed integer</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		long ReadInt64();
+	}
+
+	public static partial class IOExtensions {
+		/// <summary>
+		/// Reads a <see cref="sbyte"/> from the current position and increments <see cref="IBinaryReader.Position"/> by 1
+		/// </summary>
+		/// <returns>The 8-bit signed byte</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		public static sbyte ReadSByte(this IImageStream self) {
+			return (sbyte)self.ReadByte();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="UInt16"/> from the current position and increments <see cref="IBinaryReader.Position"/> by 2
+		/// </summary>
+		/// <returns>The 16-bit unsigned integer</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		public static ushort ReadUInt16(this IImageStream self) {
+			return (ushort)self.ReadInt16();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="UInt32"/> from the current position and increments <see cref="IBinaryReader.Position"/> by 4
+		/// </summary>
+		/// <returns>The 32-bit unsigned integer</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		public static uint ReadUInt32(this IImageStream self) {
+			return (uint)self.ReadInt32();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="UInt64"/> from the current position and increments <see cref="IBinaryReader.Position"/> by 8
+		/// </summary>
+		/// <returns>The 64-bit unsigned integer</returns>
+		/// <exception cref="IOException">An I/O error occurs</exception>
+		public static ulong ReadUInt64(this IImageStream self) {
+			return (ulong)self.ReadInt64();
+		}
+	}
+}
