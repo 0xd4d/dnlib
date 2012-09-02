b378ee71ec708252fb4887cac019512b769dce27
de4dot <de4dot@gmail.com>
2012-09-03 01:27:51 +0200

    Add AssemblyDef{User,MD}

--- dot10/dotNET/Types/AssemblyDef.cs
+++ dot10/dotNET/Types/AssemblyDef.cs
@@ -1,47 +1,19 @@
-﻿#pragma warning disable 0169	//TODO:
-
-using System;
+﻿using System;
 
 namespace dot10.dotNET.Types {
 	/// <summary>
 	/// A high-level representation of a row in the Assembly table
 	/// </summary>
-	public class AssemblyDef : IHasCustomAttribute, IHasDeclSecurity {
+	public abstract class AssemblyDef : IHasCustomAttribute, IHasDeclSecurity {
 		/// <summary>
 		/// The row id in its table
 		/// </summary>
 		protected uint rid;
 
 		/// <summary>
-		/// From column Assembly.HashAlgId
+		/// The manifest module
 		/// </summary>
-		uint hashAlgId;
-
-		/// <summary>
-		/// From columns Assembly.MajorVersion, Assembly.MinorVersion, Assembly.BuildNumber,
-		/// Assembly.RevisionNumber
-		/// </summary>
-		Version version;
-
-		/// <summary>
-		/// From column Assembly.Flags
-		/// </summary>
-		uint flags;
-
-		/// <summary>
-		/// From column Assembly.PublicKey
-		/// </summary>
-		byte[] publicKey;
-
-		/// <summary>
-		/// From column Assembly.Name
-		/// </summary>
-		string name;
-
-		/// <summary>
-		/// From column Assembly.Locale
-		/// </summary>
-		string locale;
+		protected ModuleDef manifestModule;
 
 		/// <inheritdoc/>
 		public MDToken MDToken {
@@ -56,6 +28,98 @@ namespace dot10.dotNET.Types {
 		/// <inheritdoc/>
 		public int HasDeclSecurityTag {
 			get { return 2; }
+		}
+
+		/// <summary>
+		/// From column Assembly.HashAlgId
+		/// </summary>
+		public abstract AssemblyHashAlgorithm HashAlgId { get; set; }
+
+		/// <summary>
+		/// From columns Assembly.MajorVersion, Assembly.MinorVersion, Assembly.BuildNumber,
+		/// Assembly.RevisionNumber.
+		/// </summary>
+		/// <exception cref="ArgumentNullException">If <paramref name="value"/> is null</exception>
+		public abstract Version Version { get; set; }
+
+		/// <summary>
+		/// From column Assembly.Flags
+		/// </summary>
+		public abstract AssemblyFlags Flags { get; set; }
+
+		/// <summary>
+		/// From column Assembly.PublicKey
+		/// </summary>
+		public abstract byte[] PublicKey { get; set; }
+
+		/// <summary>
+		/// From column Assembly.Name
+		/// </summary>
+		public abstract UTF8String Name { get; set; }
+
+		/// <summary>
+		/// From column Assembly.Locale
+		/// </summary>
+		public abstract UTF8String Locale { get; set; }
+
+		/// <summary>
+		/// Gets/sets the manifest module
+		/// </summary>
+		public ModuleDef ManifestModule {
+			get { return manifestModule; }
+			set { manifestModule = value; }
+		}
+	}
+
+	/// <summary>
+	/// An Assembly row created by the user and not present in the original .NET file
+	/// </summary>
+	public class AssemblyDefUser : AssemblyDef {
+		AssemblyHashAlgorithm hashAlgId;
+		Version version;
+		AssemblyFlags flags;
+		byte[] publicKey;
+		UTF8String name;
+		UTF8String locale;
+
+		/// <inheritdoc/>
+		public override AssemblyHashAlgorithm HashAlgId {
+			get { return hashAlgId; }
+			set { hashAlgId = value; }
+		}
+
+		/// <inheritdoc/>
+		public override Version Version {
+			get { return version; }
+			set {
+				if (value == null)
+					throw new ArgumentNullException("value");
+				version = value;
+			}
+		}
+
+		/// <inheritdoc/>
+		public override AssemblyFlags Flags {
+			get { return flags; }
+			set { flags = value; }
+		}
+
+		/// <inheritdoc/>
+		public override byte[] PublicKey {
+			get { return publicKey; }
+			set { publicKey = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Name {
+			get { return name; }
+			set { name = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Locale {
+			get { return locale; }
+			set { locale = value; }
 		}
 	}
 }
---
+++ dot10/dotNET/Types/AssemblyDefMD.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace dot10.dotNET.Types {
+	/// <summary>
+	/// Created from a row in the Assembly table
+	/// </summary>
+	sealed class AssemblyDefMD : AssemblyDef {
+		/// <summary>The .NET metadata where this instance is located</summary>
+		IMetaData metaData;
+		/// <summary>The raw table row. It's null until <see cref="InitializeRawRow"/> is called</summary>
+		RawAssemblyRow rawRow;
+		UserValue<AssemblyHashAlgorithm> hashAlgId;
+		UserValue<Version> version;
+		UserValue<AssemblyFlags> flags;
+		UserValue<byte[]> publicKey;
+		UserValue<UTF8String> name;
+		UserValue<UTF8String> locale;
+
+		/// <inheritdoc/>
+		public override AssemblyHashAlgorithm HashAlgId {
+			get { return hashAlgId.Value; }
+			set { hashAlgId.Value = value; }
+		}
+
+		/// <inheritdoc/>
+		public override Version Version {
+			get { return version.Value; }
+			set {
+				if (value == null)
+					throw new ArgumentNullException("value");
+				version.Value = value;
+			}
+		}
+
+		/// <inheritdoc/>
+		public override AssemblyFlags Flags {
+			get { return flags.Value; }
+			set { flags.Value = value; }
+		}
+
+		/// <inheritdoc/>
+		public override byte[] PublicKey {
+			get { return publicKey.Value; }
+			set { publicKey.Value = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Name {
+			get { return name.Value; }
+			set { name.Value = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Locale {
+			get { return locale.Value; }
+			set { locale.Value = value; }
+		}
+
+		public AssemblyDefMD(IMetaData metaData, uint rid) {
+			if (metaData == null)
+				throw new ArgumentException("metaData");
+			if (rid > 0x00FFFFFF)
+				throw new ArgumentException("rid");
+			this.rid = rid;
+			this.metaData = metaData;
+			if (metaData.TablesStream.GetTable(Table.Assembly).Rows < rid)
+				throw new BadImageFormatException(string.Format("Assembly rid {0} does not exist", rid));
+			Initialize();
+		}
+
+		void Initialize() {
+			hashAlgId = new UserValue<AssemblyHashAlgorithm> {
+				ReadOriginalValue = () => {
+					InitializeRawRow();
+					return (AssemblyHashAlgorithm)rawRow.HashAlgId;
+				}
+			};
+			version = new UserValue<Version> {
+				ReadOriginalValue = () => {
+					InitializeRawRow();
+					return new Version(rawRow.MajorVersion, rawRow.MinorVersion, rawRow.BuildNumber, rawRow.RevisionNumber);
+				}
+			};
+			flags = new UserValue<AssemblyFlags> {
+				ReadOriginalValue = () => {
+					InitializeRawRow();
+					return (AssemblyFlags)rawRow.Flags;
+				}
+			};
+			publicKey = new UserValue<byte[]> {
+				ReadOriginalValue = () => {
+					InitializeRawRow();
+					return metaData.BlobStream.Read(rawRow.PublicKey);
+				}
+			};
+			name = new UserValue<UTF8String> {
+				ReadOriginalValue = () => {
+					InitializeRawRow();
+					return metaData.StringsStream.Read(rawRow.Name);
+				}
+			};
+			locale = new UserValue<UTF8String> {
+				ReadOriginalValue = () => {
+					InitializeRawRow();
+					return metaData.StringsStream.Read(rawRow.Locale);
+				}
+			};
+		}
+
+		void InitializeRawRow() {
+			if (rawRow != null)
+				return;
+			rawRow = metaData.TablesStream.ReadAssemblyRow(rid);
+		}
+	}
+}
