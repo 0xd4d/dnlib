b509ed08a07b2157c9d2fc19893d3c8a5b717db9
de4dot <de4dot@gmail.com>
2012-09-03 00:18:05 +0200

    Add DotNetStream.ReadCompressedUInt32() method

--- dot10/dotNET/DotNetStream.cs
+++ dot10/dotNET/DotNetStream.cs
@@ -76,5 +76,48 @@ namespace dot10.dotNET {
 				return IsValidOffset(offset);
 			return size > 0 && (long)offset + (uint)size <= imageStream.Length;
 		}
+
+		/// <summary>
+		/// Reads a compressed <see cref="uint"/> from the current position in <see cref="imageStream"/>
+		/// </summary>
+		/// <remarks>Max value it can return is <c>0x1FFFFFFF</c></remarks>
+		/// <param name="val">Decompressed value</param>
+		/// <returns><c>true</c> if successful, <c>false</c> on failure</returns>
+		protected bool ReadCompressedUInt32(out uint val) {
+			var pos = imageStream.Position;
+			var len = imageStream.Length;
+			if (pos >= len) {
+				val = 0;
+				return false;
+			}
+
+			byte b = imageStream.ReadByte();
+			if ((b & 0x80) == 0) {
+				val = b;
+				return true;
+			}
+
+			if ((b & 0xC0) == 0x80) {
+				if (pos + 1 < pos || pos + 1 >= len) {
+					val = 0;
+					return false;
+				}
+				val = (uint)(((b & 0x3F) << 8) | imageStream.ReadByte());
+				return true;
+			}
+
+			if ((b & 0xE0) == 0xC0) {
+				if (pos + 3 < pos || pos + 3 >= len) {
+					val = 0;
+					return false;
+				}
+				val = (uint)(((b & 0x1F) << 24) | (imageStream.ReadByte() << 16) |
+						(imageStream.ReadByte() << 8) | imageStream.ReadByte());
+				return true;
+			}
+
+			val = 0;
+			return false;
+		}
 	}
 }
