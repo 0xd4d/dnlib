c3f03d8f0a83ee180ae546e30cad276a9bd34e28
de4dot <de4dot@gmail.com>
2012-09-02 08:17:46 +0200

    Add a method to read a string from #Strings

--- dot10/dotNET/StringsStream.cs
+++ dot10/dotNET/StringsStream.cs
@@ -1,10 +1,23 @@
-﻿using dot10.IO;
+﻿using System.Text;
+using dot10.IO;
 
 namespace dot10.dotNET {
 	class StringsStream : DotNetStream {
 		/// <inheritdoc/>
 		public StringsStream(IImageStream imageStream, StreamHeader streamHeader)
 			: base(imageStream, streamHeader) {
+		}
+
+		/// <summary>
+		/// Read a <see cref="string"/>
+		/// </summary>
+		/// <param name="offset">Offset of string</param>
+		/// <returns>The UTF-8 decoded string or null if invalid offset</returns>
+		public string Read(uint offset) {
+			var data = imageStream.ReadBytesUntilByte(0);
+			if (data == null)
+				return null;
+			return Encoding.UTF8.GetString(data);
 		}
 	}
 }
