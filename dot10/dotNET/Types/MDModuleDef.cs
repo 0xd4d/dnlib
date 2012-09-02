c94a9599aacbe9042de880d96e84f9a58f4d3e00
de4dot <de4dot@gmail.com>
2012-09-02 08:45:49 +0200

    Add methods to create a ModuleDef

---
+++ dot10/dotNET/Types/MDModuleDef.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace dot10.dotNET.Types {
+	/// <summary>
+	/// Created from a row in the Module table
+	/// </summary>
+	sealed class MDModuleDef : ModuleDef {
+		DotNetFile dnFile;
+
+		/// <summary>
+		/// Creates a <see cref="MDModuleDef"/> instance from a file
+		/// </summary>
+		/// <param name="fileName">File name of an existing .NET module/assembly</param>
+		/// <returns>A new <see cref="MDModuleDef"/> instance</returns>
+		public new static MDModuleDef Load(string fileName) {
+			DotNetFile dnFile = null;
+			try {
+				return Load(dnFile = DotNetFile.Load(fileName));
+			}
+			catch {
+				if (dnFile != null)
+					dnFile.Dispose();
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Creates a <see cref="MDModuleDef"/> instance from a byte[]
+		/// </summary>
+		/// <param name="data">Contents of a .NET module/assembly</param>
+		/// <returns>A new <see cref="MDModuleDef"/> instance</returns>
+		public new static MDModuleDef Load(byte[] data) {
+			DotNetFile dnFile = null;
+			try {
+				return Load(dnFile = DotNetFile.Load(data));
+			}
+			catch {
+				if (dnFile != null)
+					dnFile.Dispose();
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Creates a <see cref="MDModuleDef"/> instance from a memory location
+		/// </summary>
+		/// <param name="addr">Address of a .NET module/assembly</param>
+		/// <returns>A new <see cref="MDModuleDef"/> instance</returns>
+		public new static MDModuleDef Load(IntPtr addr) {
+			DotNetFile dnFile = null;
+			try {
+				return Load(dnFile = DotNetFile.Load(addr));
+			}
+			catch {
+				if (dnFile != null)
+					dnFile.Dispose();
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Creates a <see cref="MDModuleDef"/> instance from a <see cref="DotNetFile"/>
+		/// </summary>
+		/// <param name="dnFile">The loaded .NET file</param>
+		/// <returns>A new <see cref="MDModuleDef"/> instance that now owns <paramref name="dnFile"/></returns>
+		public new static MDModuleDef Load(DotNetFile dnFile) {
+			return new MDModuleDef(dnFile);
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="dnFile">The loaded .NET file</param>
+		/// <exception cref="ArgumentNullException">If <paramref name="dnFile"/> is null</exception>
+		MDModuleDef(DotNetFile dnFile) {
+			if (dnFile == null)
+				throw new ArgumentNullException("dnFile");
+
+			this.dnFile = dnFile;
+			this.rid = 1;
+			if (dnFile.MetaData.TablesStream.GetTable(Table.Module).Rows < this.rid)
+				this.rid = 0;
+		}
+
+		/// <inheritdoc/>
+		protected override void Dispose(bool disposing) {
+			if (disposing) {
+				if (dnFile != null)
+					dnFile.Dispose();
+				dnFile = null;
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
--- dot10/dotNET/Types/ModuleDef.cs
+++ dot10/dotNET/Types/ModuleDef.cs
@@ -4,7 +4,7 @@ namespace dot10.dotNET.Types {
 	/// <summary>
 	/// A high-level representation of a row in the Module table
 	/// </summary>
-	public class ModuleDef : IHasCustomAttribute, IResolutionScope {
+	public class ModuleDef : IHasCustomAttribute, IResolutionScope, IDisposable {
 		/// <summary>
 		/// The row id in its table
 		/// </summary>
@@ -23,17 +23,17 @@ namespace dot10.dotNET.Types {
 		/// <summary>
 		/// From column Module.Mvid
 		/// </summary>
-		protected Guid mvid;
+		protected Guid? mvid;
 
 		/// <summary>
 		/// From column Module.EncId
 		/// </summary>
-		protected Guid encId;
+		protected Guid? encId;
 
 		/// <summary>
 		/// From column Module.EncBaseId
 		/// </summary>
-		protected Guid encBaseId;
+		protected Guid? encBaseId;
 
 		/// <inheritdoc/>
 		public MDToken MDToken {
@@ -48,6 +48,56 @@ namespace dot10.dotNET.Types {
 		/// <inheritdoc/>
 		public int ResolutionScopeTag {
 			get { return 0; }
+		}
+
+		/// <summary>
+		/// Creates a <see cref="ModuleDef"/> instance from a file
+		/// </summary>
+		/// <param name="fileName">File name of an existing .NET module/assembly</param>
+		/// <returns>A new <see cref="ModuleDef"/> instance</returns>
+		public static ModuleDef Load(string fileName) {
+			return MDModuleDef.Load(fileName);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="ModuleDef"/> instance from a byte[]
+		/// </summary>
+		/// <param name="data">Contents of a .NET module/assembly</param>
+		/// <returns>A new <see cref="ModuleDef"/> instance</returns>
+		public static ModuleDef Load(byte[] data) {
+			return MDModuleDef.Load(data);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="ModuleDef"/> instance from a memory location
+		/// </summary>
+		/// <param name="addr">Address of a .NET module/assembly</param>
+		/// <returns>A new <see cref="ModuleDef"/> instance</returns>
+		public static ModuleDef Load(IntPtr addr) {
+			return MDModuleDef.Load(addr);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="ModuleDef"/> instance from a <see cref="DotNetFile"/>
+		/// </summary>
+		/// <param name="dnFile">The loaded .NET file</param>
+		/// <returns>A new <see cref="ModuleDef"/> instance that now owns <paramref name="dnFile"/></returns>
+		public static ModuleDef Load(DotNetFile dnFile) {
+			return MDModuleDef.Load(dnFile);
+		}
+
+
+		/// <inheritdoc/>
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Dispose method
+		/// </summary>
+		/// <param name="disposing">true if called by Dispose()</param>
+		protected virtual void Dispose(bool disposing) {
 		}
 	}
 }
