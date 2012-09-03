759e40f55bbfef055ba4d96636c3c091bcf0c5d9
de4dot <de4dot@gmail.com>
2012-09-04 00:30:32 +0200

    Add TypeDefUser and TypeDefMD classes

--- dot10/dotNET/Types/TypeDef.cs
+++ dot10/dotNET/Types/TypeDef.cs
@@ -1,46 +1,14 @@
-﻿#pragma warning disable 0169	//TODO:
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace dot10.dotNET.Types {
 	/// <summary>
 	/// A high-level representation of a row in the TypeDef table
 	/// </summary>
-	public class TypeDef : ITypeDefOrRef, IHasCustomAttribute, IHasDeclSecurity, IMemberRefParent, ITypeOrMethodDef {
+	public abstract class TypeDef : ITypeDefOrRef, IHasCustomAttribute, IHasDeclSecurity, IMemberRefParent, ITypeOrMethodDef {
 		/// <summary>
 		/// The row id in its table
 		/// </summary>
 		protected uint rid;
-
-		/// <summary>
-		/// From column TypeDef.Flags
-		/// </summary>
-		uint flags;
-
-		/// <summary>
-		/// From column TypeDef.Name
-		/// </summary>
-		string name;
-
-		/// <summary>
-		/// From column TypeDef.Namespace
-		/// </summary>
-		string @namespace;
-
-		/// <summary>
-		/// From column TypeDef.Extends
-		/// </summary>
-		ITypeDefOrRef extends;
-
-		/// <summary>
-		/// From column TypeDef.FieldList
-		/// </summary>
-		IList<FieldDef> fieldList;
-
-		/// <summary>
-		/// From column TypeDef.MethodList
-		/// </summary>
-		IList<MethodDef> methodList;
 
 		/// <inheritdoc/>
 		public MDToken MDToken {
@@ -70,6 +38,82 @@ namespace dot10.dotNET.Types {
 		/// <inheritdoc/>
 		public int TypeOrMethodDefTag {
 			get { return 0; }
+		}
+
+		/// <summary>
+		/// From column TypeDef.Flags
+		/// </summary>
+		public abstract TypeAttributes Flags { get; set; }
+
+		/// <summary>
+		/// From column TypeDef.Name
+		/// </summary>
+		public abstract UTF8String Name { get; set; }
+
+		/// <summary>
+		/// From column TypeDef.Namespace
+		/// </summary>
+		public abstract UTF8String Namespace { get; set; }
+
+		/// <summary>
+		/// From column TypeDef.Extends
+		/// </summary>
+		public abstract ITypeDefOrRef Extends { get; set; }
+
+		/// <summary>
+		/// From column TypeDef.FieldList
+		/// </summary>
+		public abstract IList<FieldDef> Fields { get; }
+
+		/// <summary>
+		/// From column TypeDef.MethodList
+		/// </summary>
+		public abstract IList<MethodDef> Methods { get; }
+	}
+
+	/// <summary>
+	/// A TypeDef row created by the user and not present in the original .NET file
+	/// </summary>
+	public class TypeDefUser : TypeDef {
+		TypeAttributes flags;
+		UTF8String name;
+		UTF8String @namespace;
+		ITypeDefOrRef extends;
+		IList<FieldDef> fields = new List<FieldDef>();
+		IList<MethodDef> methods = new List<MethodDef>();
+
+		/// <inheritdoc/>
+		public override TypeAttributes Flags {
+			get { return flags; }
+			set { flags = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Name {
+			get { return name; }
+			set { name = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Namespace {
+			get { return @namespace; }
+			set { @namespace = value; }
+		}
+
+		/// <inheritdoc/>
+		public override ITypeDefOrRef Extends {
+			get { return extends; }
+			set { extends = value; }
+		}
+
+		/// <inheritdoc/>
+		public override IList<FieldDef> Fields {
+			get { return fields; }
+		}
+
+		/// <inheritdoc/>
+		public override IList<MethodDef> Methods {
+			get { return methods; }
 		}
 	}
 }
---
+++ dot10/dotNET/Types/TypeDefMD.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dot10.dotNET.Types {
+	/// <summary>
+	/// Created from a row in the TypeDef table
+	/// </summary>
+	class TypeDefMD : TypeDef {
+		/// <summary>The .NET metadata where this instance is located</summary>
+		IMetaData metaData;
+		/// <summary>The raw table row. It's null until <see cref="InitializeRawRow"/> is called</summary>
+		RawTypeDefRow rawRow;
+
+		UserValue<TypeAttributes> flags;
+		UserValue<UTF8String> name;
+		UserValue<UTF8String> @namespace;
+		UserValue<ITypeDefOrRef> extends;
+
+		/// <inheritdoc/>
+		public override TypeAttributes Flags {
+			get { return flags.Value; }
+			set { flags.Value = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Name {
+			get { return name.Value; }
+			set { name.Value = value; }
+		}
+
+		/// <inheritdoc/>
+		public override UTF8String Namespace {
+			get { return @namespace.Value; }
+			set { @namespace.Value = value; }
+		}
+
+		/// <inheritdoc/>
+		public override ITypeDefOrRef Extends {
+			get { throw new System.NotImplementedException();/*TODO*/ }
+			set { throw new System.NotImplementedException();/*TODO*/ }
+		}
+
+		/// <inheritdoc/>
+		public override IList<FieldDef> Fields {
+			get { throw new System.NotImplementedException();/*TODO*/ }
+		}
+
+		/// <inheritdoc/>
+		public override IList<MethodDef> Methods {
+			get { throw new System.NotImplementedException();/*TODO*/ }
+		}
+
+		public TypeDefMD(IMetaData metaData, uint rid) {
+			if (metaData == null)
+				throw new ArgumentNullException("metaData");
+			if (rid == 0 || rid > 0x00FFFFFF)
+				throw new ArgumentException("rid");
+			this.rid = rid;
+			this.metaData = metaData;
+#if DEBUG
+			if (metaData.TablesStream.Get(Table.TypeDef).Rows < rid)
+				throw new BadImageFormatException(string.Format("TypeDef rid {0} does not exist", rid));
+#endif
+			Initialize();
+		}
+
+		void Initialize() {
+			flags.ReadOriginalValue = () => {
+				InitializeRawRow();
+				return (TypeAttributes)rawRow.Flags;
+			};
+			name.ReadOriginalValue = () => {
+				InitializeRawRow();
+				return metaData.StringsStream.Read(rawRow.Name);
+			};
+			@namespace.ReadOriginalValue = () => {
+				InitializeRawRow();
+				return metaData.StringsStream.Read(rawRow.Namespace);
+			};
+			extends.ReadOriginalValue = () => {
+				InitializeRawRow();
+				throw new NotImplementedException();//TODO:
+			};
+		}
+
+		void InitializeRawRow() {
+			if (rawRow != null)
+				return;
+			rawRow = metaData.TablesStream.ReadTypeDefRow(rid);
+		}
+	}
+}
