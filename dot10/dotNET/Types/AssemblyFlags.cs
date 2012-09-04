42328aaeaed8f299310f544e6bf9501a96216a29
de4dot <de4dot@gmail.com>
2012-09-04 13:40:00 +0200

    Align enum values

--- dot10/dotNET/Types/AssemblyFlags.cs
+++ dot10/dotNET/Types/AssemblyFlags.cs
@@ -7,52 +7,48 @@ namespace dot10.dotNET.Types {
 	/// <remarks>See CorHdr.h/CorAssemblyFlags</remarks>
 	[Flags]
 	public enum AssemblyFlags : uint {
-		/// <summary>
-		/// No flags set
-		/// </summary>
-		None = 0,
+		/// <summary>No flags set</summary>
+		None						= 0,
 
-		/// <summary>
-		/// The assembly ref holds the full (unhashed) public key.
-		/// </summary>
-		PublicKey = 1,
+		/// <summary>The assembly ref holds the full (unhashed) public key.</summary>
+		PublicKey					= 1,
 
 		/// <summary>Processor Architecture unspecified</summary>
-		PA_None = 0x0000,
+		PA_None						= 0x0000,
 		/// <summary>Processor Architecture: neutral (PE32)</summary>
-		PA_MSIL = 0x0010,
+		PA_MSIL						= 0x0010,
 		/// <summary>Processor Architecture: x86 (PE32)</summary>
-		PA_x86 = 0x0020,
+		PA_x86						= 0x0020,
 		/// <summary>Processor Architecture: Itanium (PE32+)</summary>
-		PA_IA64 = 0x0030,
+		PA_IA64						= 0x0030,
 		/// <summary>Processor Architecture: AMD X64 (PE32+)</summary>
-		PA_AMD64 = 0x0040,
+		PA_AMD64					= 0x0040,
 		/// <summary>Processor Architecture: ARM (PE32)</summary>
-		PA_ARM = 0x0050,
+		PA_ARM						= 0x0050,
 		/// <summary>applies to any platform but cannot run on any (e.g. reference assembly), should not have "specified" set</summary>
-		PA_NoPlatform = 0x0070,
+		PA_NoPlatform				= 0x0070,
 		/// <summary>Propagate PA flags to AssemblyRef record</summary>
-		PA_Specified = 0x0080,
+		PA_Specified				= 0x0080,
 		/// <summary>Bits describing the processor architecture</summary>
-		PA_Mask = 0x0070,
+		PA_Mask						= 0x0070,
 		/// <summary>Bits describing the PA incl. Specified</summary>
-		PA_FullMask = 0x00F0,
+		PA_FullMask					= 0x00F0,
 		/// <summary>NOT A FLAG, shift count in PA flags &lt;--&gt; index conversion</summary>
-		PA_Shift = 0x0004,
+		PA_Shift					= 0x0004,
 
 		/// <summary>From "DebuggableAttribute".</summary>
-		EnableJITcompileTracking = 0x8000,
+		EnableJITcompileTracking	= 0x8000,
 		/// <summary>From "DebuggableAttribute".</summary>
-		DisableJITcompileOptimizer = 0x4000,
+		DisableJITcompileOptimizer	= 0x4000,
 
 		/// <summary>The assembly can be retargeted (at runtime) to an assembly from a different publisher.</summary>
-		Retargetable = 0x0100,
+		Retargetable				= 0x0100,
 
 		/// <summary></summary>
-		ContentType_Default = 0x0000,
+		ContentType_Default			= 0x0000,
 		/// <summary></summary>
-		ContentType_WindowsRuntime = 0x0200,
+		ContentType_WindowsRuntime	= 0x0200,
 		/// <summary>Bits describing ContentType</summary>
-		ContentType_Mask = 0x0E00,
+		ContentType_Mask			= 0x0E00,
 	}
 }
--- dot10/dotNET/Types/AssemblyHashAlgorithm.cs
+++ dot10/dotNET/Types/AssemblyHashAlgorithm.cs
@@ -4,30 +4,30 @@
 	/// </summary>
 	public enum AssemblyHashAlgorithm : uint {
 		/// <summary></summary>
-		None = 0,
+		None				= 0,
 		/// <summary></summary>
-		MD2 = 0x8001,
+		MD2					= 0x8001,
 		/// <summary></summary>
-		MD4 = 0x8002,
+		MD4					= 0x8002,
 		/// <summary>This is a reserved value in the CLI</summary>
-		MD5 = 0x8003,
+		MD5					= 0x8003,
 		/// <summary>The only algorithm supported by the CLI</summary>
-		SHA1 = 0x8004,
+		SHA1				= 0x8004,
 		/// <summary></summary>
-		MAC = 0x8005,
+		MAC					= 0x8005,
 		/// <summary></summary>
-		SSL3_SHAMD5 = 0x8008,
+		SSL3_SHAMD5			= 0x8008,
 		/// <summary></summary>
-		HMAC = 0x8009,
+		HMAC				= 0x8009,
 		/// <summary></summary>
-		TLS1PRF = 0x800A,
+		TLS1PRF				= 0x800A,
 		/// <summary></summary>
-		HASH_REPLACE_OWF = 0x800B,
+		HASH_REPLACE_OWF	= 0x800B,
 		/// <summary></summary>
-		SHA_256 = 0x800C,
+		SHA_256				= 0x800C,
 		/// <summary></summary>
-		SHA_384 = 0x800D,
+		SHA_384				= 0x800D,
 		/// <summary></summary>
-		SHA_512 = 0x800E,
+		SHA_512				= 0x800E,
 	}
 }
