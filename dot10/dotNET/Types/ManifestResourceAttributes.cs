b118e65d36c993788476ceb033d6354c0203a27a
de4dot <de4dot@gmail.com>
2012-09-04 11:02:57 +0200

    Add ManifestResourceAttributes enum

---
+++ dot10/dotNET/Types/ManifestResourceAttributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dot10.dotNET.Types {
+	/// <summary>
+	/// ManifestResource flags. See CorHdr.h/CorManifestResourceFlags
+	/// </summary>
+	[Flags]
+	public enum ManifestResourceAttributes : uint {
+		/// <summary></summary>
+		VisibilityMask	= 0x0007,
+		/// <summary>The Resource is exported from the Assembly.</summary>
+		Public			= 0x0001,
+		/// <summary>The Resource is private to the Assembly.</summary>
+		Private			= 0x0002,
+	}
+}
