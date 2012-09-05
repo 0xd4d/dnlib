4aba669cf1044f0fadc47a88e55b24dff8e512ae
de4dot <de4dot@gmail.com>
2012-09-05 21:01:42 +0200

    Add IFullName interface

---
+++ dot10/dotNET/Types/IFullName.cs
@@ -0,0 +1,14 @@
+﻿namespace dot10.dotNET.Types {
+	interface IFullName {
+		/// <summary>
+		/// Returns the human readable full name of this type
+		/// </summary>
+		string FullName { get; }
+
+		/// <summary>
+		/// Returns the reflection name of this type. It can be passed to
+		/// <see cref="System.Type.GetType(string)"/> to load the type.
+		/// </summary>
+		string ReflectionFullName { get; }
+	}
+}
