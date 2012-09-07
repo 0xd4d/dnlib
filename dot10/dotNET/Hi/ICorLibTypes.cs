9d1210bfa4dfd42ffe8f15b7284a55d0f25f61af
de4dot <de4dot@gmail.com>
2012-09-07 11:36:40 +0200

    Change prop type to CorLibTypeSig

--- dot10/dotNET/Hi/ICorLibTypes.cs
+++ dot10/dotNET/Hi/ICorLibTypes.cs
@@ -6,92 +6,92 @@
 		/// <summary>
 		/// Gets a <c>System.Void</c>
 		/// </summary>
-		ITypeSig Void { get; }
+		CorLibTypeSig Void { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Boolean</c>
 		/// </summary>
-		ITypeSig Boolean { get; }
+		CorLibTypeSig Boolean { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Char</c>
 		/// </summary>
-		ITypeSig Char { get; }
+		CorLibTypeSig Char { get; }
 
 		/// <summary>
 		/// Gets a <c>System.SByte</c>
 		/// </summary>
-		ITypeSig SByte { get; }
+		CorLibTypeSig SByte { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Byte</c>
 		/// </summary>
-		ITypeSig Byte { get; }
+		CorLibTypeSig Byte { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Int16</c>
 		/// </summary>
-		ITypeSig Int16 { get; }
+		CorLibTypeSig Int16 { get; }
 
 		/// <summary>
 		/// Gets a <c>System.UInt16</c>
 		/// </summary>
-		ITypeSig UInt16 { get; }
+		CorLibTypeSig UInt16 { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Int32</c>
 		/// </summary>
-		ITypeSig Int32 { get; }
+		CorLibTypeSig Int32 { get; }
 
 		/// <summary>
 		/// Gets a <c>System.UInt32</c>
 		/// </summary>
-		ITypeSig UInt32 { get; }
+		CorLibTypeSig UInt32 { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Int64</c>
 		/// </summary>
-		ITypeSig Int64 { get; }
+		CorLibTypeSig Int64 { get; }
 
 		/// <summary>
 		/// Gets a <c>System.UInt64</c>
 		/// </summary>
-		ITypeSig UInt64 { get; }
+		CorLibTypeSig UInt64 { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Single</c>
 		/// </summary>
-		ITypeSig Single { get; }
+		CorLibTypeSig Single { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Double</c>
 		/// </summary>
-		ITypeSig Double { get; }
+		CorLibTypeSig Double { get; }
 
 		/// <summary>
 		/// Gets a <c>System.String</c>
 		/// </summary>
-		ITypeSig String { get; }
+		CorLibTypeSig String { get; }
 
 		/// <summary>
 		/// Gets a <c>System.TypedReference</c>
 		/// </summary>
-		ITypeSig TypedReference { get; }
+		CorLibTypeSig TypedReference { get; }
 
 		/// <summary>
 		/// Gets a <c>System.IntPtr</c>
 		/// </summary>
-		ITypeSig IntPtr { get; }
+		CorLibTypeSig IntPtr { get; }
 
 		/// <summary>
 		/// Gets a <c>System.UIntPtr</c>
 		/// </summary>
-		ITypeSig UIntPtr { get; }
+		CorLibTypeSig UIntPtr { get; }
 
 		/// <summary>
 		/// Gets a <c>System.Object</c>
 		/// </summary>
-		ITypeSig Object { get; }
+		CorLibTypeSig Object { get; }
 
 		/// <summary>
 		/// Gets the assembly reference to the core library
