82246edfe1ca8edd2c1e5bf7118f903635a433ac
de4dot <de4dot@gmail.com>
2012-09-09 08:01:42 +0200

    Update MemberRef

--- dot10/dotNET/ModuleDefMD.cs
+++ dot10/dotNET/ModuleDefMD.cs
@@ -534,7 +534,7 @@ namespace dot10.dotNET {
 		/// <param name="rid">The row ID</param>
 		/// <returns>A <see cref="MemberRefMD"/> instance or null if <paramref name="rid"/> is invalid</returns>
 		MemberRefMD ReadMemberRef(uint rid) {
-			throw new NotImplementedException();	//TODO:
+			return new MemberRefMD(this, rid);
 		}
 
 		/// <summary>
