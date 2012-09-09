1a36e006815b5b02ac445e184083729f944d940e
de4dot <de4dot@gmail.com>
2012-09-09 07:39:13 +0200

    Update MethodSpec

--- dot10/dotNET/ModuleDefMD.cs
+++ dot10/dotNET/ModuleDefMD.cs
@@ -651,7 +651,7 @@ namespace dot10.dotNET {
 		/// <param name="rid">The row ID</param>
 		/// <returns>A <see cref="MethodSpecMD"/> instance or null if <paramref name="rid"/> is invalid</returns>
 		MethodSpecMD ReadMethodSpec(uint rid) {
-			throw new NotImplementedException();	//TODO:
+			return new MethodSpecMD(this, rid);
 		}
 
 		/// <summary>
