39c3244a725435972a65bad5c5a444a172fdc02f
de4dot <de4dot@gmail.com>
2012-09-14 03:46:07 +0200

    Add XML doc comments

--- src/DotNet/Emit/OperandType.cs
+++ src/DotNet/Emit/OperandType.cs
@@ -1,45 +1,51 @@
-﻿namespace dot10.DotNet.Emit {
+﻿using dot10.DotNet.MD;
+
+namespace dot10.DotNet.Emit {
 	/// <summary>
 	/// CIL opcode operand type
 	/// </summary>
 	public enum OperandType : byte {
-		/// <summary></summary>
+		/// <summary>4-byte relative instruction offset</summary>
 		InlineBrTarget,
-		/// <summary></summary>
+		/// <summary>4-byte field token (<see cref="Table.Field"/> or <see cref="Table.MemberRef"/>)</summary>
 		InlineField,
-		/// <summary></summary>
+		/// <summary>int32</summary>
 		InlineI,
-		/// <summary></summary>
+		/// <summary>int64</summary>
 		InlineI8,
-		/// <summary></summary>
+		/// <summary>4-byte method token (<see cref="Table.Method"/>, <see cref="Table.MemberRef"/>
+		/// or <see cref="Table.MethodSpec"/>)</summary>
 		InlineMethod,
-		/// <summary></summary>
+		/// <summary>No operand</summary>
 		InlineNone,
-		/// <summary></summary>
+		/// <summary>Never used</summary>
 		InlinePhi,
-		/// <summary></summary>
+		/// <summary>64-bit real</summary>
 		InlineR,
 		/// <summary></summary>
 		NOT_USED_8,
-		/// <summary></summary>
+		/// <summary>4-byte method sig token (<see cref="Table.StandAloneSig"/>)</summary>
 		InlineSig,
-		/// <summary></summary>
+		/// <summary>4-byte string token (<c>0x70xxxxxx</c>)</summary>
 		InlineString,
-		/// <summary></summary>
+		/// <summary>4-byte count N followed by N 4-byte relative instruction offsets</summary>
 		InlineSwitch,
-		/// <summary></summary>
+		/// <summary>4-byte token (<see cref="Table.Field"/>, <see cref="Table.MemberRef"/>,
+		/// <see cref="Table.Method"/>, <see cref="Table.MethodSpec"/>, <see cref="Table.TypeDef"/>,
+		/// <see cref="Table.TypeRef"/> or <see cref="Table.TypeSpec"/>)</summary>
 		InlineTok,
-		/// <summary></summary>
+		/// <summary>4-byte type token (<see cref="Table.TypeDef"/>, <see cref="Table.TypeRef"/> or
+		/// <see cref="Table.TypeSpec"/>)</summary>
 		InlineType,
-		/// <summary></summary>
+		/// <summary>2-byte param/local index</summary>
 		InlineVar,
-		/// <summary></summary>
+		/// <summary>1-byte relative instruction offset</summary>
 		ShortInlineBrTarget,
-		/// <summary></summary>
+		/// <summary>1-byte sbyte (<see cref="Code.Ldc_I4_S"/>) or byte (the rest)</summary>
 		ShortInlineI,
-		/// <summary></summary>
+		/// <summary>32-bit real</summary>
 		ShortInlineR,
-		/// <summary></summary>
+		/// <summary>1-byte param/local index</summary>
 		ShortInlineVar,
 	}
 }
