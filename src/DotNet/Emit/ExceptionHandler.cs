38a2c2c11134183509abe73d194c8f45d2ba6b68
de4dot <de4dot@gmail.com>
2012-09-16 09:22:41 +0200

    Add try start/end, handler start/end, etc to ExceptionHandler

--- src/DotNet/Emit/ExceptionHandler.cs
+++ src/DotNet/Emit/ExceptionHandler.cs
@@ -3,5 +3,56 @@
 	/// A CIL method exception handler
 	/// </summary>
 	public class ExceptionHandler {
+		/// <summary>
+		/// First instruction of try block
+		/// </summary>
+		public Instruction TryStart;
+
+		/// <summary>
+		/// One instruction past the end of try block or <c>null</c> if it ends at the end
+		/// of the method.
+		/// </summary>
+		public Instruction TryEnd;
+
+		/// <summary>
+		/// Start of filter handler or <c>null</c> if none. The end of filter handler is
+		/// always <see cref="HandlerStart"/>.
+		/// </summary>
+		public Instruction FilterStart;
+
+		/// <summary>
+		/// First instruction of try handler block
+		/// </summary>
+		public Instruction HandlerStart;
+
+		/// <summary>
+		/// One instruction past the end of try handler block or <c>null</c> if it ends at the end
+		/// of the method.
+		/// </summary>
+		public Instruction HandlerEnd;
+
+		/// <summary>
+		/// The catch type if <see cref="HandlerType"/> is <see cref="ExceptionClause.Catch"/>
+		/// </summary>
+		public ITypeDefOrRef CatchType;
+
+		/// <summary>
+		/// Type of exception handler clause
+		/// </summary>
+		public ExceptionClause HandlerType;
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public ExceptionHandler() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="handlerType">Exception clause type</param>
+		public ExceptionHandler(ExceptionClause handlerType) {
+			this.HandlerType = handlerType;
+		}
 	}
 }
