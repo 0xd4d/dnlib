8096675f71f9155611090d765fd7c0a381842ee1
de4dot <de4dot@gmail.com>
2012-09-16 05:44:45 +0200

    Add a method body reader

---
+++ src/DotNet/Emit/MethodBodyReader.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using dot10.IO;
+using dot10.DotNet.MD;
+
+namespace dot10.DotNet.Emit {
+	/// <summary>
+	/// Reads a .NET method body (header, locals, instructions, exception handlers)
+	/// </summary>
+	sealed class MethodBodyReader : MethodBodyReaderBase {
+		readonly ModuleDefMD module;
+		ushort flags;
+		ushort maxStack;
+		uint codeSize;
+		uint localVarSigTok;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="module">The reader module</param>
+		/// <param name="reader">A reader positioned at the start of a .NET method body</param>
+		/// <param name="method">Use parameters from this method</param>
+		public MethodBodyReader(ModuleDefMD module, IBinaryReader reader, MethodDef method)
+			: this(module, reader, method.Parameters.Parameters) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="module">The reader module</param>
+		/// <param name="reader">A reader positioned at the start of a .NET method body</param>
+		/// <param name="parameters">Method parameters</param>
+		public MethodBodyReader(ModuleDefMD module, IBinaryReader reader, IList<Parameter> parameters)
+			: base(reader, parameters) {
+			this.module = module;
+		}
+
+		/// <summary>
+		/// Reads the method body header, all instructions, and the exception handlers (if any)
+		/// </summary>
+		/// <exception cref="InvalidMethodException">If it's an invalid method body. It's not thrown
+		/// if an invalid instruction is found.</exception>
+		/// <exception cref="OutOfMemoryException">If we can't allocate enough memory</exception>
+		public void Read() {
+			try {
+				ReadHeader();
+				SetLocals(ReadLocals());
+				ReadInstructions();
+				ReadExceptionHandlers();
+			}
+			catch (OutOfMemoryException) {
+				throw;
+			}
+			catch (InvalidMethodException) {
+				throw;
+			}
+			catch (Exception ex) {
+				throw new InvalidMethodException("Could not read method", ex);
+			}
+		}
+
+		/// <summary>
+		/// Reads the method header
+		/// </summary>
+		/// <exception cref="InvalidMethodException">If the header is invalid</exception>
+		void ReadHeader() {
+			byte b = reader.ReadByte();
+			switch (b & 7) {
+			case 2:
+			case 6:
+				// Tiny header. [7:2] = code size, max stack is 8, no locals or exception handlers
+				flags = 2;
+				maxStack = 8;
+				codeSize = (uint)(b >> 2);
+				localVarSigTok = 0;
+				break;
+
+			case 3:
+				// Fat header. Can have locals and exception handlers
+				flags = (ushort)((reader.ReadByte() << 8) | b);
+				if ((flags >> 12) < 4)
+					throw new InvalidMethodException("Header size in dwords < 4");
+				maxStack = reader.ReadUInt16();
+				codeSize = reader.ReadUInt32();
+				localVarSigTok = reader.ReadUInt32();
+				reader.Position += ((flags >> 12) - 4) * 4;
+				break;
+
+			default:
+				throw new InvalidMethodException("It's not a tiny or a fat method header");
+			}
+
+			if (reader.Position + codeSize < reader.Position || reader.Position + codeSize > reader.Length)
+				throw new InvalidMethodException("Invalid code size");
+		}
+
+		/// <summary>
+		/// Reads the locals
+		/// </summary>
+		/// <exception cref="InvalidMethodException">If <see cref="localVarSigTok"/> is invalid</exception>
+		/// <returns>All locals or null if there are none</returns>
+		IList<ITypeSig> ReadLocals() {
+			if (localVarSigTok == 0)
+				return null;
+			if (MDToken.ToTable(localVarSigTok) != Table.StandAloneSig)
+				throw new InvalidMethodException(string.Format("LocalVarSigTok ({0:X8}) is not a StandAloneSig token", localVarSigTok));
+			var standAloneSig = module.ResolveStandAloneSig(MDToken.ToRID(localVarSigTok));
+			if (standAloneSig == null)
+				throw new InvalidMethodException(string.Format("LocalVarSigTok ({0:X8}) has an invalid RID", localVarSigTok));
+			var localSig = standAloneSig.LocalSig;
+			if (localSig == null)
+				throw new InvalidMethodException(string.Format("LocalVarSigTok ({0:X8}) isn't a locals sig", localVarSigTok));
+			return localSig.Locals;
+		}
+
+		/// <summary>
+		/// Reads all instructions
+		/// </summary>
+		void ReadInstructions() {
+			ReadInstructionsNumBytes(codeSize);
+		}
+
+		/// <inheritdoc/>
+		protected override IField ReadInlineField(Instruction instr) {
+			return module.ResolveToken(reader.ReadUInt32()) as IField;
+		}
+
+		/// <inheritdoc/>
+		protected override IMethod ReadInlineMethod(Instruction instr) {
+			return module.ResolveToken(reader.ReadUInt32()) as IMethod;
+		}
+
+		/// <inheritdoc/>
+		protected override MethodSig ReadInlineSig(Instruction instr) {
+			uint token = reader.ReadUInt32();
+			if (MDToken.ToTable(token) != Table.StandAloneSig)
+				return null;
+			var standAloneSig = module.ResolveStandAloneSig(MDToken.ToRID(token));
+			if (standAloneSig == null)
+				return null;
+			return standAloneSig.MethodSig;
+		}
+
+		/// <inheritdoc/>
+		protected override string ReadInlineString(Instruction instr) {
+			return module.USStream.Read(reader.ReadUInt32() & 0x00FFFFFF);
+		}
+
+		/// <inheritdoc/>
+		protected override ITokenOperand ReadInlineTok(Instruction instr) {
+			return module.ResolveToken(reader.ReadUInt32()) as ITokenOperand;
+		}
+
+		/// <inheritdoc/>
+		protected override ITypeDefOrRef ReadInlineType(Instruction instr) {
+			return module.ResolveToken(reader.ReadUInt32()) as ITypeDefOrRef;
+		}
+
+		/// <summary>
+		/// Reads all exception handlers
+		/// </summary>
+		void ReadExceptionHandlers() {
+			if ((flags & 8) == 0)
+				return;
+			reader.Position = (reader.Position + 3) & ~3;
+			//TODO:
+		}
+	}
+}
---
+++ src/DotNet/Emit/MethodBodyReaderBase.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using dot10.IO;
+
+namespace dot10.DotNet.Emit {
+	/// <summary>
+	/// Method body reader base class
+	/// </summary>
+	public abstract class MethodBodyReaderBase {
+		/// <summary>The method reader</summary>
+		protected IBinaryReader reader;
+		/// <summary>All parameters</summary>
+		protected IList<Parameter> parameters;
+		/// <summary>All locals</summary>
+		protected IList<Local> locals = new List<Local>();
+		/// <summary>All instructions</summary>
+		protected IList<Instruction> instructions;
+		/// <summary>All exception handlers</summary>
+		protected IList<ExceptionHandler> exceptionHandlers = new List<ExceptionHandler>();
+		uint currentOffset;
+		/// <summary>First byte after the end of the code</summary>
+		protected long codeEndOffs;
+
+		/// <summary>
+		/// Gets all parameters
+		/// </summary>
+		public IList<Parameter> Parameters {
+			get { return parameters; }
+		}
+
+		/// <summary>
+		/// Gets all locals
+		/// </summary>
+		public IList<Local> Locals {
+			get { return locals; }
+		}
+
+		/// <summary>
+		/// Gets all instructions
+		/// </summary>
+		public IList<Instruction> Instructions {
+			get { return instructions; }
+		}
+
+		/// <summary>
+		/// Gets all exception handlers
+		/// </summary>
+		public IList<ExceptionHandler> ExceptionHandlers {
+			get { return exceptionHandlers; }
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="reader">The reader</param>
+		/// <param name="parameters">Method parameters</param>
+		protected MethodBodyReaderBase(IBinaryReader reader, IList<Parameter> parameters) {
+			this.reader = reader;
+			this.parameters = parameters;
+		}
+
+		/// <summary>
+		/// Sets new locals
+		/// </summary>
+		/// <param name="newLocals">A list of types of all locals or <c>null</c> if none</param>
+		protected void SetLocals(IList<ITypeSig> newLocals) {
+			locals.Clear();
+			if (newLocals == null)
+				return;
+			foreach (var typeSig in newLocals)
+				locals.Add(new Local(typeSig));
+		}
+
+		/// <summary>
+		/// Reads all instructions
+		/// </summary>
+		/// <param name="numInstrs">Number of instructions to read</param>
+		protected void ReadInstructions(int numInstrs) {
+			codeEndOffs = reader.Length;	// We don't know the end pos so use the last one
+			instructions = new List<Instruction>(numInstrs);
+			currentOffset = 0;
+			for (int i = 0; i < instructions.Count; i++)
+				instructions[i] = ReadOneInstruction();
+			FixBranches();
+		}
+
+		/// <summary>
+		/// Reads all instructions
+		/// </summary>
+		/// <param name="codeSize">Size of code</param>
+		protected void ReadInstructionsNumBytes(uint codeSize) {
+			codeEndOffs = reader.Position + codeSize;
+			if (codeEndOffs < reader.Position || codeEndOffs > reader.Length)
+				throw new InvalidMethodException("Invalid code size");
+
+			instructions = new List<Instruction>();	//TODO: Estimate number of instructions based on codeSize
+			currentOffset = 0;
+			while (reader.Position < codeEndOffs)
+				instructions.Add(ReadOneInstruction());
+			reader.Position = codeEndOffs;
+			FixBranches();
+		}
+
+		/// <summary>
+		/// Fixes all branch instructions so their operands are set to an <see cref="Instruction"/>
+		/// instead of an offset.
+		/// </summary>
+		void FixBranches() {
+			foreach (var instr in instructions) {
+				switch (instr.OpCode.OperandType) {
+				case OperandType.InlineBrTarget:
+				case OperandType.ShortInlineBrTarget:
+					instr.Operand = GetInstruction((uint)instr.Operand);
+					break;
+
+				case OperandType.InlineSwitch:
+					var uintTargets = (IList<uint>)instr.Operand;
+					var targets = new Instruction[uintTargets.Count];
+					for (int i = 0; i < uintTargets.Count; i++)
+						targets[i] = GetInstruction(uintTargets[i]);
+					instr.Operand = targets;
+					break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Finds an instruction
+		/// </summary>
+		/// <param name="offset">Offset of instruction</param>
+		/// <returns>The instruction or <c>null</c> if there's no instruction at <paramref name="offset"/>.</returns>
+		Instruction GetInstruction(uint offset) {
+			// The instructions are sorted and all Offset fields are correct. Do a binary search.
+			int lo = 0, hi = instructions.Count - 1;
+			while (lo <= hi) {
+				int i = (lo + hi) / 2;
+				var instr = instructions[i];
+				if (instr.Offset == offset)
+					return instr;
+				if (offset < instr.Offset)
+					hi = i - 1;
+				else
+					lo = i + 1;
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// Reads the next instruction
+		/// </summary>
+		Instruction ReadOneInstruction() {
+			var instr = new Instruction();
+			instr.Offset = currentOffset;
+			instr.OpCode = ReadOpCode();
+			instr.Operand = ReadOperand(instr);
+
+			if (instr.OpCode.Code == Code.Switch) {
+				IList<uint> targets = (IList<uint>)instr.Operand;
+				currentOffset += (uint)(instr.OpCode.Size + 4 + 4 * targets.Count);
+			}
+			else
+				currentOffset += (uint)instr.GetSize();
+			return instr;
+		}
+
+		/// <summary>
+		/// Reads the next OpCode from the current position
+		/// </summary>
+		OpCode ReadOpCode() {
+			var op = reader.ReadByte();
+			if (op != 0xFE)
+				return OpCodes.OneByteOpCodes[op];
+			return OpCodes.TwoByteOpCodes[reader.ReadByte()];
+		}
+
+		/// <summary>
+		/// Reads the instruction operand (if any)
+		/// </summary>
+		/// <param name="instr">The instruction</param>
+		object ReadOperand(Instruction instr) {
+			switch (instr.OpCode.OperandType) {
+			case OperandType.InlineBrTarget:	return ReadInlineBrTarget(instr);
+			case OperandType.InlineField:		return ReadInlineField(instr);
+			case OperandType.InlineI:			return ReadInlineI(instr);
+			case OperandType.InlineI8:			return ReadInlineI8(instr);
+			case OperandType.InlineMethod:		return ReadInlineMethod(instr);
+			case OperandType.InlineNone:		return ReadInlineNone(instr);
+			case OperandType.InlinePhi:			return ReadInlinePhi(instr);
+			case OperandType.InlineR:			return ReadInlineR(instr);
+			case OperandType.InlineSig:			return ReadInlineSig(instr);
+			case OperandType.InlineString:		return ReadInlineString(instr);
+			case OperandType.InlineSwitch:		return ReadInlineSwitch(instr);
+			case OperandType.InlineTok:			return ReadInlineTok(instr);
+			case OperandType.InlineType:		return ReadInlineType(instr);
+			case OperandType.InlineVar:			return ReadInlineVar(instr);
+			case OperandType.ShortInlineBrTarget: return ReadShortInlineBrTarget(instr);
+			case OperandType.ShortInlineI:		return ReadShortInlineI(instr);
+			case OperandType.ShortInlineR:		return ReadShortInlineR(instr);
+			case OperandType.ShortInlineVar:	return ReadShortInlineVar(instr);
+			default: throw new InvalidOperationException("Invalid OpCode.OperandType");
+			}
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineBrTarget"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual uint ReadInlineBrTarget(Instruction instr) {
+			return instr.Offset + (uint)instr.GetSize() + reader.ReadUInt32();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineField"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected abstract IField ReadInlineField(Instruction instr);
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineI"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual int ReadInlineI(Instruction instr) {
+			return reader.ReadInt32();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineI8"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual long ReadInlineI8(Instruction instr) {
+			return reader.ReadInt64();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineMethod"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected abstract IMethod ReadInlineMethod(Instruction instr);
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineNone"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual object ReadInlineNone(Instruction instr) {
+			return null;
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlinePhi"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual object ReadInlinePhi(Instruction instr) {
+			return null;
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineR"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual double ReadInlineR(Instruction instr) {
+			return reader.ReadDouble();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineSig"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected abstract MethodSig ReadInlineSig(Instruction instr);
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineString"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected abstract string ReadInlineString(Instruction instr);
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineSwitch"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual IList<uint> ReadInlineSwitch(Instruction instr) {
+			var num = reader.ReadUInt32();
+			long offsetAfterInstr = (long)instr.Offset + (long)instr.OpCode.Size + 4L + (long)num * 4;
+			if (offsetAfterInstr < instr.Offset || offsetAfterInstr > codeEndOffs) {
+				reader.Position = codeEndOffs;
+				return new uint[0];
+			}
+
+			var targets = new uint[num];
+			uint offset = instr.Offset + (uint)instr.OpCode.Size + 4 + num * 4;
+			for (int i = 0; i < targets.Length; i++)
+				targets[i] = offset + reader.ReadUInt32();
+			return targets;
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineTok"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected abstract ITokenOperand ReadInlineTok(Instruction instr);
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineType"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected abstract ITypeDefOrRef ReadInlineType(Instruction instr);
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineVar"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual object ReadInlineVar(Instruction instr) {
+			if (IsArgOperandInstruction(instr))
+				return ReadInlineVarArg(instr);
+			return ReadInlineVarLocal(instr);
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineVar"/> (a parameter) operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual Parameter ReadInlineVarArg(Instruction instr) {
+			return GetParameter(reader.ReadUInt16());
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.InlineVar"/> (a local) operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual Local ReadInlineVarLocal(Instruction instr) {
+			return GetLocal(reader.ReadUInt16());
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.ShortInlineBrTarget"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual uint ReadShortInlineBrTarget(Instruction instr) {
+			return instr.Offset + (uint)instr.GetSize() + (uint)reader.ReadSByte();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.ShortInlineI"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual object ReadShortInlineI(Instruction instr) {
+			if (instr.OpCode.Code == Code.Ldc_I4_S)
+				return reader.ReadSByte();
+			return reader.ReadByte();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.ShortInlineR"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual float ReadShortInlineR(Instruction instr) {
+			return reader.ReadSingle();
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.ShortInlineVar"/> operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual object ReadShortInlineVar(Instruction instr) {
+			if (IsArgOperandInstruction(instr))
+				return ReadShortInlineVarArg(instr);
+			return ReadShortInlineVarLocal(instr);
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.ShortInlineVar"/> (a parameter) operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual Parameter ReadShortInlineVarArg(Instruction instr) {
+			return GetParameter(reader.ReadByte());
+		}
+
+		/// <summary>
+		/// Reads a <see cref="OperandType.ShortInlineVar"/> (a local) operand
+		/// </summary>
+		/// <param name="instr">The current instruction</param>
+		/// <returns>The operand</returns>
+		protected virtual Local ReadShortInlineVarLocal(Instruction instr) {
+			return GetLocal(reader.ReadByte());
+		}
+
+		/// <summary>
+		/// Returns <c>true</c> if it's one of the ldarg/starg instructions that have an operand
+		/// </summary>
+		/// <param name="instr">The instruction to check</param>
+		protected static bool IsArgOperandInstruction(Instruction instr) {
+			switch (instr.OpCode.Code) {
+			case Code.Ldarg:
+			case Code.Ldarg_S:
+			case Code.Ldarga:
+			case Code.Ldarga_S:
+			case Code.Starg:
+			case Code.Starg_S:
+				return true;
+			default:
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Returns a parameter
+		/// </summary>
+		/// <param name="index">A parameter index</param>
+		/// <returns>A <see cref="Parameter"/> or <c>null</c> if <paramref name="index"/> is invalid</returns>
+		protected Parameter GetParameter(int index) {
+			if ((uint)index >= parameters.Count)
+				return null;
+			return parameters[index];
+		}
+
+		/// <summary>
+		/// Returns a local
+		/// </summary>
+		/// <param name="index">A local index</param>
+		/// <returns>A <see cref="Local"/> or <c>null</c> if <paramref name="index"/> is invalid</returns>
+		protected Local GetLocal(int index) {
+			if ((uint)index >= locals.Count)
+				return null;
+			return locals[index];
+		}
+	}
+}
