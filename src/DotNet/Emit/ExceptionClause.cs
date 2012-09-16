8605f9bd73aef140ff472268240605a23641bc21
de4dot <de4dot@gmail.com>
2012-09-16 09:22:07 +0200

    Add ExceptionClause enum

---
+++ src/DotNet/Emit/ExceptionClause.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dot10.DotNet.Emit {
+	/// <summary>
+	/// Type of exception handler. See CorHdr.h/CorExceptionFlag
+	/// </summary>
+	[Flags]
+	public enum ExceptionClause {
+		/// <summary></summary>
+		Catch		= 0x0000,
+		/// <summary></summary>
+		Filter		= 0x0001,
+		/// <summary></summary>
+		Finally		= 0x0002,
+		/// <summary></summary>
+		Fault		= 0x0004,
+		/// <summary></summary>
+		Duplicated	= 0x0008,
+	}
+}
