1aac51f073fcabf52b4cd9898bdfd941292ee4ee
de4dot <de4dot@gmail.com>
2012-09-16 05:31:41 +0200

    Add a ITypeSig field to Local

--- src/DotNet/Emit/Local.cs
+++ src/DotNet/Emit/Local.cs
@@ -2,6 +2,23 @@
 	/// <summary>
 	/// A method local
 	/// </summary>
-	public class Local {
+	public sealed class Local {
+		ITypeSig typeSig;
+
+		/// <summary>
+		/// Gets/sets the type of the local
+		/// </summary>
+		public ITypeSig Type {
+			get { return typeSig; }
+			set { typeSig = value; }
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="typeSig">The type</param>
+		public Local(ITypeSig typeSig) {
+			this.typeSig = typeSig;
+		}
 	}
 }
