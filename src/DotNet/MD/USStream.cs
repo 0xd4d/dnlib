850c7860d656eec1bc002ed7728557bbabde7cc7
de4dot <de4dot@gmail.com>
2012-09-16 05:19:05 +0200

    Add USStream.Read() method

--- src/DotNet/MD/USStream.cs
+++ src/DotNet/MD/USStream.cs
@@ -9,5 +9,34 @@ namespace dot10.DotNet.MD {
 		public USStream(IImageStream imageStream, StreamHeader streamHeader)
 			: base(imageStream, streamHeader) {
 		}
+
+		/// <summary>
+		/// Reads a unicode string
+		/// </summary>
+		/// <param name="offset">Offset of unicode string</param>
+		/// <returns>A string or <c>null</c> if <paramref name="offset"/> is invalid</returns>
+		public string Read(uint offset) {
+			if (offset == 0)
+				return string.Empty;
+			if (!IsValidOffset(offset))
+				return null;
+			imageStream.Position = offset;
+			uint length;
+			if (!imageStream.ReadCompressedUInt32(out length))
+				return null;
+			if (imageStream.Position + length < length || imageStream.Position + length > imageStream.Length)
+				return null;
+			return imageStream.ReadString((int)(length / 2));
+		}
+
+		/// <summary>
+		/// Reads data just like <see cref="Read"/>, but returns an empty string if
+		/// offset is invalid
+		/// </summary>
+		/// <param name="offset">Offset of unicode string</param>
+		/// <returns>The string</returns>
+		public string ReadNoNull(uint offset) {
+			return Read(offset) ?? string.Empty;
+		}
 	}
 }
