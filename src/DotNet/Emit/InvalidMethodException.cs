0cfde537661eaba73e3fc8a4213e5eef0eeea5e0
de4dot <de4dot@gmail.com>
2012-09-16 05:43:41 +0200

    Add InvalidMethodException class

---
+++ src/DotNet/Emit/InvalidMethodException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace dot10.DotNet.Emit {
+	/// <summary>
+	/// Thrown when invalid data is detected while parsing a .NET method
+	/// </summary>
+	[Serializable]
+	public class InvalidMethodException : Exception {
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public InvalidMethodException() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="msg">Error message</param>
+		public InvalidMethodException(string msg)
+			: base(msg) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="msg">Error message</param>
+		/// <param name="innerException">The inner exception or <c>null</c> if none</param>
+		public InvalidMethodException(string msg, Exception innerException)
+			: base(msg, innerException) {
+		}
+	}
+}
