e01b5b224cafef1c8746871d9b1768d029c81f29
de4dot <de4dot@gmail.com>
2012-09-22 13:21:01 +0200

    Add IncrementRecursionCounterThrow() which throws InfiniteRecursionException

--- src/DotNet/RecursionCounter.cs
+++ src/DotNet/RecursionCounter.cs
@@ -1,6 +1,32 @@
 ﻿using System;
 
 namespace dot10.DotNet {
+	[Serializable]
+	class InfiniteRecursionException : Exception {
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public InfiniteRecursionException() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="msg">Error message</param>
+		public InfiniteRecursionException(string msg)
+			: base(msg) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="msg">Error message</param>
+		/// <param name="innerException">Other exception</param>
+		public InfiniteRecursionException(string msg, Exception innerException)
+			: base(msg, innerException) {
+		}
+	}
+
 	struct RecursionCounter {
 		/// <summary>
 		/// Max recursion count. If this is reached, we won't continue, and will use a default value.
@@ -30,8 +56,22 @@ namespace dot10.DotNet {
 		}
 
 		/// <summary>
+		/// Increments <see cref="counter"/> if it's not too high. <c>ALL</c> instance methods
+		/// that can be called recursively must call this method and <see cref="DecrementRecursionCounter"/>
+		/// (if this method returns <c>true</c>)
+		/// </summary>
+		/// <exception cref="InfiniteRecursionException"><see cref="IncrementRecursionCounterThrow"/>
+		/// has been called too many times</exception>
+		public void IncrementRecursionCounterThrow() {
+			if (counter >= MAX_RECURSION_COUNT)
+				throw new InfiniteRecursionException("Infinite recursion");
+			counter++;
+		}
+
+		/// <summary>
 		/// Must be called before returning to caller if <see cref="IncrementRecursionCounter"/>
-		/// returned <c>true</c>
+		/// returned <c>true</c> or if <see cref="IncrementRecursionCounterThrow"/> didn't throw an
+		/// exception.
 		/// </summary>
 		public void DecrementRecursionCounter() {
 #if DEBUG
