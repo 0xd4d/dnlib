d98e09015af987aaec77dc7bcc46172ead314762
de4dot <de4dot@gmail.com>
2012-09-30 22:33:17 +0200

    Add AssemblyResolver

---
+++ src/DotNet/AssemblyResolver.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using dot10.DotNet.MD;
+
+namespace dot10.DotNet {
+	/// <summary>
+	/// Resolves assemblies
+	/// </summary>
+	public class AssemblyResolver : IAssemblyResolver {
+		static readonly ModuleDef nullModule = new ModuleDefUser();
+
+		// DLL files are searched before EXE files
+		static readonly IList<string> assemblyExtensions = new string[] { ".dll", ".exe" };
+
+		static readonly GacInfo gac2Info;	// .NET 1.x and 2.x
+		static readonly GacInfo gac4Info;	// .NET 4.x
+
+		Dictionary<ModuleDef, List<string>> moduleSearchPaths = new Dictionary<ModuleDef, List<string>>();
+		Dictionary<string, AssemblyDef> cachedAssemblies = new Dictionary<string, AssemblyDef>(StringComparer.Ordinal);
+		List<string> preSearchPaths = new List<string>();
+		List<string> postSearchPaths = new List<string>();
+		bool findExactMatch;
+
+		class GacInfo {
+			public string path;
+			public string prefix;
+			public IList<string> subDirs;
+
+			public GacInfo(string prefix, string path, IList<string> subDirs) {
+				this.prefix = prefix;
+				this.path = path;
+				this.subDirs = subDirs;
+			}
+		}
+
+		static AssemblyResolver() {
+			var windir = Environment.GetEnvironmentVariable("WINDIR");
+			if (!string.IsNullOrEmpty(windir)) {
+				gac2Info = new GacInfo("", Path.Combine(windir, "assembly"), new string[] {
+					"GAC_MSIL", "GAC_32", "GAC_64", "GAC"
+				});
+				gac4Info = new GacInfo("v4.0_", Path.Combine(Path.Combine(windir, "Microsoft.NET"), "assembly"), new string[] {
+					"GAC_MSIL", "GAC_32", "GAC_64"
+				});
+			}
+		}
+
+		/// <summary>
+		/// <c>true</c> if <see cref="Resolve"/> should find an assembly that matches exactly.
+		/// <c>false</c> if it first tries to match exactly, and if that fails, it picks an
+		/// assembly that is closest to the requested assembly.
+		/// </summary>
+		public bool FindExactMatch {
+			get { return findExactMatch; }
+			set { findExactMatch = value; }
+		}
+
+		/// <summary>
+		/// Gets paths searched before trying the standard locations
+		/// </summary>
+		public IList<string> PreSearchPaths {
+			get { return preSearchPaths; }
+		}
+
+		/// <summary>
+		/// Gets paths searched after trying the standard locations
+		/// </summary>
+		public IList<string> PostSearchPaths {
+			get { return postSearchPaths; }
+		}
+
+		/// <inheritdoc/>
+		public AssemblyDef Resolve(AssemblyNameInfo assembly, ModuleDef sourceModule) {
+			if (assembly == null)
+				return null;
+
+			AssemblyDef resolvedAssembly = Resolve2(assembly, sourceModule);
+			if (resolvedAssembly != null) {
+				AssemblyDef cachedAssembly;
+				var resolvedAsmKey = GetAssemblyNameKey(new AssemblyNameInfo(resolvedAssembly));
+				if (cachedAssemblies.TryGetValue(resolvedAsmKey, out cachedAssembly) && cachedAssembly != resolvedAssembly) {
+					if (resolvedAssembly.ManifestModule != null)
+						resolvedAssembly.ManifestModule.Dispose();
+					resolvedAssembly = cachedAssembly;
+				}
+				cachedAssemblies.Add(GetAssemblyNameKey(assembly), resolvedAssembly);
+				cachedAssemblies[resolvedAsmKey] = resolvedAssembly;
+			}
+
+			return resolvedAssembly;
+		}
+
+		static string GetAssemblyNameKey(AssemblyNameInfo asmName) {
+			// Make sure the name contains PublicKeyToken= and not PublicKey=
+			return asmName.FullNameToken;
+		}
+
+		AssemblyDef Resolve2(AssemblyNameInfo assembly, ModuleDef sourceModule) {
+			AssemblyDef resolvedAssembly;
+
+			if (cachedAssemblies.TryGetValue(GetAssemblyNameKey(assembly), out resolvedAssembly))
+				return resolvedAssembly;
+
+			var asmSimpleName = UTF8String.ToSystemStringOrEmpty(assembly.Name);
+			resolvedAssembly = FindExactAssembly(assembly, PreFindAssemblies(assembly, sourceModule, true)) ??
+					FindExactAssembly(assembly, FindAssemblies(assembly, sourceModule, true)) ??
+					FindExactAssembly(assembly, PostFindAssemblies(assembly, sourceModule, true));
+			if (resolvedAssembly != null)
+				return resolvedAssembly;
+
+			if (!findExactMatch) {
+				resolvedAssembly = FindClosestAssembly(assembly);
+				resolvedAssembly = FindClosestAssembly(assembly, resolvedAssembly, PreFindAssemblies(assembly, sourceModule, false));
+				resolvedAssembly = FindClosestAssembly(assembly, resolvedAssembly, FindAssemblies(assembly, sourceModule, false));
+				resolvedAssembly = FindClosestAssembly(assembly, resolvedAssembly, PostFindAssemblies(assembly, sourceModule, false));
+			}
+
+			return resolvedAssembly;
+		}
+
+		/// <summary>
+		/// Finds an assembly that exactly matches the requested assembly
+		/// </summary>
+		/// <param name="assembly">Assembly name to find</param>
+		/// <param name="paths">Search paths or <c>null</c> if none</param>
+		/// <returns>An <see cref="AssemblyDef"/> instance or <c>null</c> if an exact match
+		/// couldn't be found.</returns>
+		AssemblyDef FindExactAssembly(AssemblyNameInfo assembly, IEnumerable<string> paths) {
+			if (paths == null)
+				return null;
+			var asmComparer = new AssemblyNameComparer(AssemblyNameComparerFlags.All);
+			foreach (var path in paths) {
+				ModuleDefMD mod = null;
+				try {
+					mod = ModuleDefMD.Load(path);
+					var asm = mod.Assembly;
+					if (asm != null && asmComparer.Equals(assembly, new AssemblyNameInfo(asm))) {
+						mod = null;
+						return asm;
+					}
+				}
+				catch {
+				}
+				finally {
+					if (mod != null)
+						mod.Dispose();
+				}
+			}
+			return null;
+		}
+
+		AssemblyDef FindClosestAssembly(AssemblyNameInfo assembly) {
+			AssemblyDef closest = null;
+			var asmComparer = new AssemblyNameComparer(AssemblyNameComparerFlags.All);
+			foreach (var asm in cachedAssemblies.Values) {
+				if (asmComparer.CompareClosest(assembly, new AssemblyNameInfo(closest), new AssemblyNameInfo(asm)) == 1)
+					closest = asm;
+			}
+			return closest;
+		}
+
+		AssemblyDef FindClosestAssembly(AssemblyNameInfo assembly, AssemblyDef closest, IEnumerable<string> paths) {
+			if (paths == null)
+				return closest;
+			var asmComparer = new AssemblyNameComparer(AssemblyNameComparerFlags.All);
+			foreach (var path in paths) {
+				ModuleDefMD mod = null;
+				try {
+					mod = ModuleDefMD.Load(path);
+					var asm = mod.Assembly;
+					if (asm != null && asmComparer.CompareClosest(assembly, new AssemblyNameInfo(closest), new AssemblyNameInfo(asm)) == 1) {
+						if (!IsCached(closest) && closest != null && closest.ManifestModule != null)
+							closest.ManifestModule.Dispose();
+						closest = asm;
+						mod = null;
+					}
+				}
+				catch {
+				}
+				finally {
+					if (mod != null)
+						mod.Dispose();
+				}
+			}
+
+			return closest;
+		}
+
+		/// <summary>
+		/// Returns <c>true</c> if <paramref name="asm"/> is inserted in <see cref="cachedAssemblies"/>
+		/// </summary>
+		/// <param name="asm">Assembly to check</param>
+		bool IsCached(AssemblyDef asm) {
+			if (asm == null)
+				return false;
+			AssemblyDef cachedAsm;
+			return cachedAssemblies.TryGetValue(GetAssemblyNameKey(new AssemblyNameInfo(asm)), out cachedAsm) &&
+					cachedAsm == asm;
+		}
+
+		IEnumerable<string> FindAssemblies2(AssemblyNameInfo assembly, IEnumerable<string> paths) {
+			if (paths != null) {
+				var asmSimpleName = UTF8String.ToSystemStringOrEmpty(assembly.Name);
+				foreach (var path in paths) {
+					foreach (var ext in assemblyExtensions) {
+						var fullPath = Path.Combine(path, asmSimpleName + ext);
+						if (File.Exists(fullPath))
+							yield return fullPath;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Called before <see cref="FindAssemblies"/>
+		/// </summary>
+		/// <param name="assembly">Simple assembly name</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <param name="matchExactly">We're trying to find an exact match</param>
+		/// <returns><c>null</c> or an enumerable of full paths to try</returns>
+		protected virtual IEnumerable<string> PreFindAssemblies(AssemblyNameInfo assembly, ModuleDef sourceModule, bool matchExactly) {
+			foreach (var path in FindAssemblies2(assembly, preSearchPaths))
+				yield return path;
+		}
+
+		/// <summary>
+		/// Called after <see cref="FindAssemblies"/> (if it fails)
+		/// </summary>
+		/// <param name="assembly">Simple assembly name</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <param name="matchExactly">We're trying to find an exact match</param>
+		/// <returns><c>null</c> or an enumerable of full paths to try</returns>
+		protected virtual IEnumerable<string> PostFindAssemblies(AssemblyNameInfo assembly, ModuleDef sourceModule, bool matchExactly) {
+			foreach (var path in FindAssemblies2(assembly, postSearchPaths))
+				yield return path;
+		}
+
+		/// <summary>
+		/// Called after <see cref="PreFindAssemblies"/> (if it fails)
+		/// </summary>
+		/// <param name="assembly">Simple assembly name</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <param name="matchExactly">We're trying to find an exact match</param>
+		/// <returns><c>null</c> or an enumerable of full paths to try</returns>
+		protected virtual IEnumerable<string> FindAssemblies(AssemblyNameInfo assembly, ModuleDef sourceModule, bool matchExactly) {
+			foreach (var path in FindAssembliesGac(assembly, sourceModule, matchExactly))
+				yield return path;
+			foreach (var path in FindAssembliesModuleSearchPaths(assembly, sourceModule, matchExactly))
+				yield return path;
+		}
+
+		IEnumerable<string> FindAssembliesGac(AssemblyNameInfo assembly, ModuleDef sourceModule, bool matchExactly) {
+			if (matchExactly)
+				return FindAssembliesGacExactly(assembly, sourceModule);
+			return FindAssembliesGacAny(assembly, sourceModule);
+		}
+
+		IEnumerable<string> FindAssembliesGacExactly(AssemblyNameInfo assembly, ModuleDef sourceModule) {
+			foreach (var path in FindAssembliesGacExactly(gac2Info, assembly, sourceModule))
+				yield return path;
+			foreach (var path in FindAssembliesGacExactly(gac4Info, assembly, sourceModule))
+				yield return path;
+		}
+
+		IEnumerable<string> FindAssembliesGacExactly(GacInfo gacInfo, AssemblyNameInfo assembly, ModuleDef sourceModule) {
+			var pkt = PublicKeyBase.ToPublicKeyToken(assembly.PublicKeyOrToken);
+			if (gacInfo != null && pkt != null) {
+				string pktString = pkt.ToString();
+				string verString = Utils.CreateVersionWithNoUndefinedValues(assembly.Version).ToString();
+				var asmSimpleName = UTF8String.ToSystemStringOrEmpty(assembly.Name);
+				foreach (var subDir in gacInfo.subDirs) {
+					var baseDir = Path.Combine(gacInfo.path, subDir);
+					baseDir = Path.Combine(baseDir, asmSimpleName);
+					baseDir = Path.Combine(baseDir, string.Format("{0}{1}__{2}", gacInfo.prefix, verString, pktString));
+					var pathName = Path.Combine(baseDir, asmSimpleName + ".dll");
+					if (File.Exists(pathName))
+						yield return pathName;
+				}
+			}
+		}
+
+		IEnumerable<string> FindAssembliesGacAny(AssemblyNameInfo assembly, ModuleDef sourceModule) {
+			foreach (var path in FindAssembliesGacAny(gac2Info, assembly, sourceModule))
+				yield return path;
+			foreach (var path in FindAssembliesGacAny(gac4Info, assembly, sourceModule))
+				yield return path;
+		}
+
+		IEnumerable<string> FindAssembliesGacAny(GacInfo gacInfo, AssemblyNameInfo assembly, ModuleDef sourceModule) {
+			if (gacInfo != null) {
+				var asmSimpleName = UTF8String.ToSystemStringOrEmpty(assembly.Name);
+				foreach (var subDir in gacInfo.subDirs) {
+					var baseDir = Path.Combine(gacInfo.path, subDir);
+					baseDir = Path.Combine(baseDir, asmSimpleName);
+					foreach (var dir in GetDirs(baseDir)) {
+						var pathName = Path.Combine(dir, asmSimpleName + ".dll");
+						if (File.Exists(pathName))
+							yield return pathName;
+					}
+				}
+			}
+		}
+
+		IEnumerable<string> GetDirs(string baseDir) {
+			var dirs = new List<string>();
+			try {
+				foreach (var di in new DirectoryInfo(baseDir).GetDirectories())
+					dirs.Add(di.FullName);
+			}
+			catch {
+			}
+			return dirs;
+		}
+
+		IEnumerable<string> FindAssembliesModuleSearchPaths(AssemblyNameInfo assembly, ModuleDef sourceModule, bool matchExactly) {
+			string asmSimpleName = UTF8String.ToSystemStringOrEmpty(assembly.Name);
+			var searchPaths = GetSearchPaths(sourceModule);
+			foreach (var ext in assemblyExtensions) {
+				foreach (var path in searchPaths) {
+					for (int i = 0; i < 2; i++) {
+						string path2;
+						if (i == 0)
+							path2 = Path.Combine(path, asmSimpleName + ext);
+						else
+							path2 = Path.Combine(Path.Combine(path, asmSimpleName), asmSimpleName + ext);
+						if (File.Exists(path2))
+							yield return path2;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets all search paths to use for this module
+		/// </summary>
+		/// <param name="module">The module or <c>null</c> if unknown</param>
+		/// <returns>A list of all search paths to use for this module</returns>
+		IEnumerable<string> GetSearchPaths(ModuleDef module) {
+			if (module == null)
+				module = nullModule;
+			List<string> searchPaths;
+			if (moduleSearchPaths.TryGetValue(module, out searchPaths))
+				return searchPaths;
+			moduleSearchPaths[module] = searchPaths = new List<string>(GetModuleSearchPaths(module));
+			return searchPaths;
+		}
+
+		/// <summary>
+		/// Gets all module search paths. This is usually empty unless its assembly has
+		/// a <c>.config</c> file specifying any additional private search paths in a
+		/// &lt;probing/&gt; element.
+		/// </summary>
+		/// <param name="module">The module or <c>null</c> if unknown</param>
+		/// <returns>A list of search paths</returns>
+		protected virtual IEnumerable<string> GetModuleSearchPaths(ModuleDef module) {
+			return GetModulePrivateSearchPaths(module);
+		}
+
+		/// <summary>
+		/// Gets all private assembly search paths as found in the module's <c>.config</c> file.
+		/// </summary>
+		/// <param name="module">The module or <c>null</c> if unknown</param>
+		/// <returns>A list of search paths</returns>
+		protected IEnumerable<string> GetModulePrivateSearchPaths(ModuleDef module) {
+			if (module == null || module.Assembly == null)
+				return new List<string>();
+			module = module.Assembly.ManifestModule;
+			if (module == null)
+				return new List<string>();	// Should never happen
+
+			try {
+				var imageName = module.Location;
+				var configName = imageName + ".config";
+				if (imageName != "" && File.Exists(configName)) {
+					var baseDir = Directory.GetParent(imageName).FullName;
+					return GetPrivatePaths(baseDir, configName);
+				}
+			}
+			catch {
+			}
+			return new List<string>();
+		}
+
+		IEnumerable<string> GetPrivatePaths(string baseDir, string configFileName) {
+			var searchPaths = new List<string>();
+
+			try {
+				var dirName = Path.GetDirectoryName(Path.GetFullPath(configFileName));
+				searchPaths.Add(dirName);
+
+				using (var xmlStream = new FileStream(configFileName, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+					var doc = new XmlDocument();
+					doc.Load(XmlReader.Create(xmlStream));
+					foreach (var tmp in doc.GetElementsByTagName("probing")) {
+						var probingElem = tmp as XmlElement;
+						if (probingElem == null)
+							continue;
+						var privatePath = probingElem.GetAttribute("privatePath");
+						if (string.IsNullOrEmpty(privatePath))
+							continue;
+						foreach (var path in privatePath.Split(';')) {
+							var newPath = Path.GetFullPath(Path.Combine(dirName, path.Replace('\\', Path.DirectorySeparatorChar)));
+							if (Directory.Exists(newPath) && newPath.Contains(baseDir + Path.DirectorySeparatorChar))
+								searchPaths.Add(newPath);
+						}
+					}
+				}
+			}
+			catch (ArgumentException) {
+			}
+			catch (IOException) {
+			}
+			catch (XmlException) {
+			}
+
+			return searchPaths;
+		}
+	}
+}
---
+++ src/DotNet/IAssemblyResolver.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Reflection;
+
+namespace dot10.DotNet {
+	/// <summary>
+	/// Thrown if any of the <c>ResolveThrow()</c> methods fail to resolve an assembly
+	/// </summary>
+	[Serializable]
+	public class AssemblyResolveException : Exception {
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public AssemblyResolveException() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="message">Exception message</param>
+		public AssemblyResolveException(string message)
+			: base(message) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="message">Exception message</param>
+		/// <param name="innerException">Inner exception or <c>null</c> if none</param>
+		public AssemblyResolveException(string message, Exception innerException)
+			: base(message, innerException) {
+		}
+	}
+
+	/// <summary>
+	/// Resolves assemblies
+	/// </summary>
+	public interface IAssemblyResolver {
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="assembly">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver or
+		/// <c>null</c> if the assembly couldn't be found.</returns>
+		AssemblyDef Resolve(AssemblyNameInfo assembly, ModuleDef sourceModule);
+	}
+
+	static partial class Extensions {
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="assembly">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver or
+		/// <c>null</c> if the assembly couldn't be found.</returns>
+		public static AssemblyDef Resolve(this IAssemblyResolver self, AssemblyName assembly, ModuleDef sourceModule) {
+			return self.Resolve(new AssemblyNameInfo(assembly), sourceModule);
+		}
+
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="asmFullName">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver or
+		/// <c>null</c> if the assembly couldn't be found.</returns>
+		public static AssemblyDef Resolve(this IAssemblyResolver self, string asmFullName, ModuleDef sourceModule) {
+			return self.Resolve(new AssemblyNameInfo(asmFullName), sourceModule);
+		}
+
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="assembly">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver or
+		/// <c>null</c> if the assembly couldn't be found.</returns>
+		public static AssemblyDef Resolve(this IAssemblyResolver self, IAssembly assembly, ModuleDef sourceModule) {
+			return self.Resolve(new AssemblyNameInfo(assembly), sourceModule);
+		}
+
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="assembly">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver</returns>
+		/// <exception cref="AssemblyResolveException">If the assembly couldn't be found.</exception>
+		public static AssemblyDef ResolveThrow(this IAssemblyResolver self, AssemblyNameInfo assembly, ModuleDef sourceModule) {
+			var asm = self.Resolve(assembly, sourceModule);
+			if (asm != null)
+				return asm;
+			throw new AssemblyResolveException(string.Format("Could not resolve {0}", assembly));
+		}
+
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="assembly">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver</returns>
+		/// <exception cref="AssemblyResolveException">If the assembly couldn't be found.</exception>
+		public static AssemblyDef ResolveThrow(this IAssemblyResolver self, AssemblyName assembly, ModuleDef sourceModule) {
+			var asm = self.Resolve(new AssemblyNameInfo(assembly), sourceModule);
+			if (asm != null)
+				return asm;
+			throw new AssemblyResolveException(string.Format("Could not resolve {0}", assembly));
+		}
+
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="asmFullName">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver</returns>
+		/// <exception cref="AssemblyResolveException">If the assembly couldn't be found.</exception>
+		public static AssemblyDef ResolveThrow(this IAssemblyResolver self, string asmFullName, ModuleDef sourceModule) {
+			var asm = self.Resolve(new AssemblyNameInfo(asmFullName), sourceModule);
+			if (asm != null)
+				return asm;
+			throw new AssemblyResolveException(string.Format("Could not resolve {0}", asmFullName));
+		}
+
+		/// <summary>
+		/// Finds and returns an <see cref="AssemblyDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="assembly">The assembly to find</param>
+		/// <param name="sourceModule">The module that needs to resolve an assembly or <c>null</c></param>
+		/// <returns>An <see cref="AssemblyDef"/> instance owned by the assembly resolver</returns>
+		/// <exception cref="AssemblyResolveException">If the assembly couldn't be found.</exception>
+		public static AssemblyDef ResolveThrow(this IAssemblyResolver self, IAssembly assembly, ModuleDef sourceModule) {
+			var asm = self.Resolve(new AssemblyNameInfo(assembly), sourceModule);
+			if (asm != null)
+				return asm;
+			throw new AssemblyResolveException(string.Format("Could not resolve {0}", assembly));
+		}
+	}
+}
