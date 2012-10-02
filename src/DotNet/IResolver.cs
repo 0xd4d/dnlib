7ffff1987086ca1d0e3e357dd6e532cec493c0f4
de4dot <de4dot@gmail.com>
2012-10-02 20:22:33 +0200

    Create more resolver extension methods

--- src/DotNet/IResolver.cs
+++ src/DotNet/IResolver.cs
@@ -10,7 +10,7 @@
 	/// </summary>
 	public interface ITypeResolver {
 		/// <summary>
-		/// Resolve a type
+		/// Resolves a type
 		/// </summary>
 		/// <param name="typeRef">The type</param>
 		/// <returns>A <see cref="TypeDef"/> instance or <c>null</c> if it couldn't be resolved</returns>
@@ -28,5 +28,83 @@
 		/// <returns>A <see cref="MethodDef"/> or a <see cref="FieldDef"/> instance or <c>null</c>
 		/// if it couldn't be resolved.</returns>
 		IMemberForwarded Resolve(MemberRef memberRef);
+	}
+
+	static partial class Extensions {
+		/// <summary>
+		/// Resolves a type
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="typeRef">The type</param>
+		/// <returns>A <see cref="TypeDef"/> instance</returns>
+		/// <exception cref="TypeResolveException">If the type couldn't be resolved</exception>
+		public static TypeDef ResolveThrow(this ITypeResolver self, TypeRef typeRef) {
+			var type = self.Resolve(typeRef);
+			if (type != null)
+				return type;
+			throw new TypeResolveException(string.Format("Could not resolve type: {0}", typeRef));
+		}
+
+		/// <summary>
+		/// Resolves a method or a field
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="memberRef">A method/field reference</param>
+		/// <returns>A <see cref="MethodDef"/> or a <see cref="FieldDef"/> instance</returns>
+		/// <exception cref="MemberRefResolveException">If the method/field couldn't be resolved</exception>
+		public static IMemberForwarded ResolveThrow(this IMemberRefResolver self, MemberRef memberRef) {
+			var memberDef = self.Resolve(memberRef);
+			if (memberDef != null)
+				return memberDef;
+			throw new MemberRefResolveException(string.Format("Could not resolve method/field: {0}", memberRef));
+		}
+
+		/// <summary>
+		/// Resolves a field
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="memberRef">A field reference</param>
+		/// <returns>A <see cref="FieldDef"/> instance or <c>null</c> if it couldn't be resolved.</returns>
+		public static FieldDef ResolveField(this IMemberRefResolver self, MemberRef memberRef) {
+			return self.Resolve(memberRef) as FieldDef;
+		}
+
+		/// <summary>
+		/// Resolves a field
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="memberRef">A field reference</param>
+		/// <returns>A <see cref="FieldDef"/> instance or <c>null</c> if it couldn't be resolved.</returns>
+		/// <exception cref="MemberRefResolveException">If the field couldn't be resolved</exception>
+		public static FieldDef ResolveFieldThrow(this IMemberRefResolver self, MemberRef memberRef) {
+			var field = self.Resolve(memberRef) as FieldDef;
+			if (field != null)
+				return field;
+			throw new MemberRefResolveException(string.Format("Could not resolve field: {0}", memberRef));
+		}
+
+		/// <summary>
+		/// Resolves a method
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="memberRef">A method reference</param>
+		/// <returns>A <see cref="MethodDef"/> instance or <c>null</c> if it couldn't be resolved.</returns>
+		public static MethodDef ResolveMethod(this IMemberRefResolver self, MemberRef memberRef) {
+			return self.Resolve(memberRef) as MethodDef;
+		}
+
+		/// <summary>
+		/// Resolves a method
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="memberRef">A method reference</param>
+		/// <returns>A <see cref="MethodDef"/> instance or <c>null</c> if it couldn't be resolved.</returns>
+		/// <exception cref="MemberRefResolveException">If the method couldn't be resolved</exception>
+		public static MethodDef ResolveMethodThrow(this IMemberRefResolver self, MemberRef memberRef) {
+			var method = self.Resolve(memberRef) as MethodDef;
+			if (method != null)
+				return method;
+			throw new MemberRefResolveException(string.Format("Could not resolve method: {0}", memberRef));
+		}
 	}
 }
--- src/DotNet/ITypeDefFinder.cs
+++ src/DotNet/ITypeDefFinder.cs
@@ -14,8 +14,8 @@
 		TypeDef Find(string fullName, bool isReflectionName);
 
 		/// <summary>
-		/// Finds a <see cref="TypeDef"/>. Its scope (i.e., module or assembly) is ignored when
-		/// looking up the type.
+		/// Finds a <see cref="TypeDef"/>. <paramref name="typeRef"/>'s scope (i.e., module or
+		/// assembly) is ignored when looking up the type.
 		/// </summary>
 		/// <param name="typeRef">The type ref</param>
 		/// <returns>An existing <see cref="TypeDef"/> or <c>null</c> if it wasn't found.</returns>
@@ -23,6 +23,38 @@
 	}
 
 	static partial class Extensions {
+		/// <summary>
+		/// Finds a <see cref="TypeDef"/>. Its scope (i.e., module or assembly) is ignored when
+		/// looking up the type.
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="typeRef">The type ref</param>
+		/// <returns>An existing <see cref="TypeDef"/> or <c>null</c> if it wasn't found.</returns>
+		/// <exception cref="TypeResolveException">If type couldn't be found</exception>
+		public static TypeDef FindThrow(this ITypeDefFinder self, TypeRef typeRef) {
+			var type = self.Find(typeRef);
+			if (type != null)
+				return type;
+			throw new TypeResolveException(string.Format("Could not find type: {0}", typeRef));
+		}
+
+		/// <summary>
+		/// Finds a <see cref="TypeDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="fullName">Full name of the type (no assembly information)</param>
+		/// <param name="isReflectionName"><c>true</c> if it's a reflection name, and nested
+		/// type names are separated by a <c>+</c> character. If <c>false</c>, nested type names
+		/// are separated by a <c>/</c> character.</param>
+		/// <returns>An existing <see cref="TypeDef"/></returns>
+		/// <exception cref="TypeResolveException">If type couldn't be found</exception>
+		public static TypeDef FindThrow(this ITypeDefFinder self, string fullName, bool isReflectionName) {
+			var type = self.Find(fullName, isReflectionName);
+			if (type != null)
+				return type;
+			throw new TypeResolveException(string.Format("Could not find type: {0}", fullName));
+		}
+
 		/// <summary>
 		/// Finds a <see cref="TypeDef"/>
 		/// </summary>
@@ -37,10 +69,49 @@
 		/// Finds a <see cref="TypeDef"/>
 		/// </summary>
 		/// <param name="self">this</param>
+		/// <param name="fullName">Full name of the type (no assembly information). Nested types are separated by <c>/</c></param>
+		/// <returns>An existing <see cref="TypeDef"/></returns>
+		/// <exception cref="TypeResolveException">If type couldn't be found</exception>
+		public static TypeDef FindNormalThrow(this ITypeDefFinder self, string fullName) {
+			var type = self.Find(fullName, false);
+			if (type != null)
+				return type;
+			throw new TypeResolveException(string.Format("Could not find type: {0}", fullName));
+		}
+
+		/// <summary>
+		/// Finds a <see cref="TypeDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
 		/// <param name="fullName">Full name of the type (no assembly information). Nested types are separated by <c>+</c></param>
 		/// <returns>An existing <see cref="TypeDef"/> or <c>null</c> if it wasn't found.</returns>
 		public static TypeDef FindReflection(this ITypeDefFinder self, string fullName) {
 			return self.Find(fullName, true);
+		}
+
+		/// <summary>
+		/// Finds a <see cref="TypeDef"/>
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="fullName">Full name of the type (no assembly information). Nested types are separated by <c>+</c></param>
+		/// <returns>An existing <see cref="TypeDef"/></returns>
+		/// <exception cref="TypeResolveException">If type couldn't be found</exception>
+		public static TypeDef FindReflectionThrow(this ITypeDefFinder self, string fullName) {
+			var type = self.Find(fullName, true);
+			if (type != null)
+				return type;
+			throw new TypeResolveException(string.Format("Could not find type: {0}", fullName));
+		}
+
+		/// <summary>
+		/// Checks whether a <see cref="TypeDef"/> exists. <paramref name="typeRef"/>'s scope (i.e.,
+		/// module or assembly) is ignored when looking up the type.
+		/// </summary>
+		/// <param name="self">this</param>
+		/// <param name="typeRef">The type ref</param>
+		/// <returns><c>true</c> if the <see cref="TypeDef"/> exists, <c>false</c> otherwise</returns>
+		public static bool TypeExists(this ITypeDefFinder self, TypeRef typeRef) {
+			return self.Find(typeRef) != null;
 		}
 
 		/// <summary>
