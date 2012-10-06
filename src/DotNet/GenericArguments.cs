ce1814d1de647c51845b7ee349f096c3791cc34b
de4dot <de4dot@gmail.com>
2012-10-06 02:43:56 +0200

    Update GenericArguments.Resolve()

--- src/DotNet/GenericArguments.cs
+++ src/DotNet/GenericArguments.cs
@@ -56,7 +56,6 @@ namespace dot10.DotNet {
 	}
 
 	class GenericArguments {
-		const int MAX_RESOLVE_TRIES = 30;
 		GenericArgumentsStack typeArgsStack = new GenericArgumentsStack(true);
 		GenericArgumentsStack methodArgsStack = new GenericArgumentsStack(false);
 
@@ -93,7 +92,9 @@ namespace dot10.DotNet {
 		}
 
 		/// <summary>
-		/// Replaces a generic type/method var with its generic argument (if any)
+		/// Replaces a generic type/method var with its generic argument (if any). If
+		/// <paramref name="typeSig"/> isn't a generic type/method var or if it can't
+		/// be resolved, it itself is returned. Else the resolved type is returned.
 		/// </summary>
 		/// <param name="typeSig">Type signature</param>
 		/// <returns>New <see cref="TypeSig"/> which is never <c>null</c> unless
@@ -101,30 +102,23 @@ namespace dot10.DotNet {
 		public TypeSig Resolve(TypeSig typeSig) {
 			if (typeSig == null)
 				return null;
+
 			var sig = typeSig;
-			for (int i = 0; i < MAX_RESOLVE_TRIES; i++) {
-				bool updated = false;
 
-				var genericVar = sig as GenericVar;
-				if (genericVar != null) {
-					var newSig = typeArgsStack.Resolve(genericVar.Number);
-					if (newSig == null || newSig == sig)
-						return sig;
-					sig = newSig;
-					updated = true;
-				}
+			var genericMVar = sig as GenericMVar;
+			if (genericMVar != null) {
+				var newSig = methodArgsStack.Resolve(genericMVar.Number);
+				if (newSig == null || newSig == sig)
+					return sig;
+				sig = newSig;
+			}
 
-				var genericMVar = sig as GenericMVar;
-				if (genericMVar != null) {
-					var newSig = methodArgsStack.Resolve(genericMVar.Number);
-					if (newSig == null || newSig == sig)
-						return sig;
-					sig = newSig;
-					updated = true;
-				}
-
-				if (!updated)
-					break;
+			var genericVar = sig as GenericVar;
+			if (genericVar != null) {
+				var newSig = typeArgsStack.Resolve(genericVar.Number);
+				if (newSig == null || newSig == sig)
+					return sig;
+				sig = newSig;
 			}
 
 			return sig;
