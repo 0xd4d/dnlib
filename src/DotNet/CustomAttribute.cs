9712e0e56c8befb9389436e59f6a28f08e114d19
de4dot <de4dot@gmail.com>
2012-10-10 19:10:54 +0200

    Add CustomAttribute classes

---
+++ src/DotNet/CustomAttribute.cs
@@ -0,0 +1,383 @@
+﻿using System.Collections.Generic;
+using dot10.DotNet.MD;
+
+namespace dot10.DotNet {
+	/// <summary>
+	/// A custom attribute
+	/// </summary>
+	public class CustomAttribute {
+		ICustomAttributeType ctor;
+		byte[] rawData;
+		List<CAArgument> arguments;
+		List<CANamedArgument> namedArguments;
+
+		/// <summary>
+		/// Gets/sets the custom attribute constructor
+		/// </summary>
+		public ICustomAttributeType Ctor {
+			get { return ctor; }
+			set { ctor = value; }
+		}
+
+		/// <summary>
+		/// <c>true</c> if the raw custom attribute blob hasn't been parsed
+		/// </summary>
+		public bool IsRawBlob {
+			get { return rawData != null; }
+		}
+
+		/// <summary>
+		/// Gets the raw custom attribute blob or <c>null</c>
+		/// </summary>
+		public byte[] RawData {
+			get { return rawData; }
+		}
+
+		/// <summary>
+		/// Gets all constructor arguments
+		/// </summary>
+		public List<CAArgument> Arguments {
+			get { return arguments; }
+		}
+
+		/// <summary>
+		/// Gets all named arguments (field and property values)
+		/// </summary>
+		public List<CANamedArgument> NamedArguments {
+			get { return namedArguments; }
+		}
+
+		/// <summary>
+		/// Gets all <see cref="CANamedArgument"/>s that are field arguments
+		/// </summary>
+		public IEnumerable<CANamedArgument> FieldArguments {
+			get {
+				foreach (var namedArg in namedArguments) {
+					if (namedArg.IsField)
+						yield return namedArg;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets all <see cref="CANamedArgument"/>s that are property arguments
+		/// </summary>
+		public IEnumerable<CANamedArgument> PropertyArguments {
+			get {
+				foreach (var namedArg in namedArguments) {
+					if (namedArg.IsProperty)
+						yield return namedArg;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="ctor">Custom attribute constructor</param>
+		/// <param name="rawData">Raw custom attribute blob</param>
+		public CustomAttribute(ICustomAttributeType ctor, byte[] rawData)
+			: this(ctor, null, null) {
+			this.rawData = rawData;
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="ctor">Custom attribute constructor</param>
+		public CustomAttribute(ICustomAttributeType ctor)
+			: this(ctor, null, null) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="ctor">Custom attribute constructor</param>
+		/// <param name="arguments">Constructor arguments or <c>null</c> if none</param>
+		public CustomAttribute(ICustomAttributeType ctor, IEnumerable<CAArgument> arguments)
+			: this(ctor, arguments, null) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="ctor">Custom attribute constructor</param>
+		/// <param name="namedArguments">Named arguments or <c>null</c> if none</param>
+		public CustomAttribute(ICustomAttributeType ctor, IEnumerable<CANamedArgument> namedArguments)
+			: this(ctor, null, namedArguments) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="ctor">Custom attribute constructor</param>
+		/// <param name="arguments">Constructor arguments or <c>null</c> if none</param>
+		/// <param name="namedArguments">Named arguments or <c>null</c> if none</param>
+		public CustomAttribute(ICustomAttributeType ctor, IEnumerable<CAArgument> arguments, IEnumerable<CANamedArgument> namedArguments) {
+			this.ctor = ctor;
+			this.arguments = arguments == null ? new List<CAArgument>() : new List<CAArgument>(arguments);
+			this.namedArguments = namedArguments == null ? new List<CANamedArgument>() : new List<CANamedArgument>(namedArguments);
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="ctor">Custom attribute constructor</param>
+		/// <param name="arguments">Constructor arguments. The list is now owned by this instance.</param>
+		/// <param name="namedArguments">Named arguments. The list is now owned by this instance.</param>
+		internal CustomAttribute(ICustomAttributeType ctor, List<CAArgument> arguments, List<CANamedArgument> namedArguments) {
+			this.ctor = ctor;
+			this.arguments = arguments;
+			this.namedArguments = namedArguments;
+		}
+
+		/// <summary>
+		/// Gets the field named <paramref name="name"/>
+		/// </summary>
+		/// <param name="name">Name of field</param>
+		/// <returns>A <see cref="CANamedArgument"/> instance or <c>null</c> if not found</returns>
+		public CANamedArgument GetField(string name) {
+			return GetNamedArgument(name, true);
+		}
+
+		/// <summary>
+		/// Gets the field named <paramref name="name"/>
+		/// </summary>
+		/// <param name="name">Name of field</param>
+		/// <returns>A <see cref="CANamedArgument"/> instance or <c>null</c> if not found</returns>
+		public CANamedArgument GetField(UTF8String name) {
+			return GetNamedArgument(name, true);
+		}
+
+		/// <summary>
+		/// Gets the property named <paramref name="name"/>
+		/// </summary>
+		/// <param name="name">Name of property</param>
+		/// <returns>A <see cref="CANamedArgument"/> instance or <c>null</c> if not found</returns>
+		public CANamedArgument GetProperty(string name) {
+			return GetNamedArgument(name, false);
+		}
+
+		/// <summary>
+		/// Gets the property named <paramref name="name"/>
+		/// </summary>
+		/// <param name="name">Name of property</param>
+		/// <returns>A <see cref="CANamedArgument"/> instance or <c>null</c> if not found</returns>
+		public CANamedArgument GetProperty(UTF8String name) {
+			return GetNamedArgument(name, false);
+		}
+
+		/// <summary>
+		/// Gets the property/field named <paramref name="name"/>
+		/// </summary>
+		/// <param name="name">Name of property/field</param>
+		/// <param name="isField"><c>true</c> if it's a field, <c>false</c> if it's a property</param>
+		/// <returns>A <see cref="CANamedArgument"/> instance or <c>null</c> if not found</returns>
+		public CANamedArgument GetNamedArgument(string name, bool isField) {
+			foreach (var namedArg in namedArguments) {
+				if (namedArg.IsField == isField && UTF8String.ToSystemStringOrEmpty(namedArg.Name) == name)
+					return namedArg;
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// Gets the property/field named <paramref name="name"/>
+		/// </summary>
+		/// <param name="name">Name of property/field</param>
+		/// <param name="isField"><c>true</c> if it's a field, <c>false</c> if it's a property</param>
+		/// <returns>A <see cref="CANamedArgument"/> instance or <c>null</c> if not found</returns>
+		public CANamedArgument GetNamedArgument(UTF8String name, bool isField) {
+			foreach (var namedArg in namedArguments) {
+				if (namedArg.IsField == isField && UTF8String.Equals(namedArg.Name, name))
+					return namedArg;
+			}
+			return null;
+		}
+
+		/// <inheritdoc/>
+		public override string ToString() {
+			var mrCtor = ctor as MemberRef;
+			if (mrCtor != null)
+				return string.Format("{0}", mrCtor.Class);
+			var mdCtor = ctor as MethodDef;
+			if (mdCtor != null)
+				return string.Format("{0}", mdCtor.DeclaringType);
+			return "???";
+		}
+	}
+
+	/// <summary>
+	/// A custom attribute constructor argument
+	/// </summary>
+	public class CAArgument {
+		TypeSig type;
+		object value;
+
+		/// <summary>
+		/// Gets/sets the argument type
+		/// </summary>
+		public TypeSig Type {
+			get { return type; }
+			set { type = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the argument value
+		/// </summary>
+		public object Value {
+			get { return value; }
+			set { this.value = value; }
+		}
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public CAArgument() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="type">Argument type</param>
+		public CAArgument(TypeSig type) {
+			this.type = type;
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="type">Argument type</param>
+		/// <param name="value">Argument value</param>
+		public CAArgument(TypeSig type, object value) {
+			this.type = type;
+			this.value = value;
+		}
+
+		/// <inheritdoc/>
+		public override string ToString() {
+			return string.Format("{0} ({1})", value, type);
+		}
+	}
+
+	/// <summary>
+	/// A custom attribute field/property argument
+	/// </summary>
+	public class CANamedArgument {
+		bool isField;
+		TypeSig type;
+		UTF8String name;
+		CAArgument argument;
+
+		/// <summary>
+		/// <c>true</c> if it's a field
+		/// </summary>
+		public bool IsField {
+			get { return isField; }
+			set { isField = value; }
+		}
+
+		/// <summary>
+		/// <c>true</c> if it's a property
+		/// </summary>
+		public bool IsProperty {
+			get { return !isField; }
+			set { isField = !value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the field/property type
+		/// </summary>
+		public TypeSig Type {
+			get { return type; }
+			set { type = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the property/field name
+		/// </summary>
+		public UTF8String Name {
+			get { return name; }
+			set { name = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the argument
+		/// </summary>
+		public CAArgument Argument {
+			get { return argument; }
+			set { argument = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the argument type
+		/// </summary>
+		public TypeSig ArgumentType {
+			get { return argument.Type; }
+			set { argument.Type = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the argument value
+		/// </summary>
+		public object Value {
+			get { return argument.Value; }
+			set { argument.Value = value; }
+		}
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public CANamedArgument() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="isField"><c>true</c> if field, <c>false</c> if property</param>
+		public CANamedArgument(bool isField) {
+			this.isField = isField;
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="isField"><c>true</c> if field, <c>false</c> if property</param>
+		/// <param name="type">Field/property type</param>
+		public CANamedArgument(bool isField, TypeSig type) {
+			this.isField = isField;
+			this.type = type;
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="isField"><c>true</c> if field, <c>false</c> if property</param>
+		/// <param name="type">Field/property type</param>
+		/// <param name="name">Name of field/property</param>
+		public CANamedArgument(bool isField, TypeSig type, UTF8String name) {
+			this.isField = isField;
+			this.type = type;
+			this.name = name;
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="isField"><c>true</c> if field, <c>false</c> if property</param>
+		/// <param name="type">Field/property type</param>
+		/// <param name="name">Name of field/property</param>
+		/// <param name="argument">Field/property argument</param>
+		public CANamedArgument(bool isField, TypeSig type, UTF8String name, CAArgument argument) {
+			this.isField = isField;
+			this.type = type;
+			this.name = name;
+			this.argument = argument;
+		}
+
+		/// <inheritdoc/>
+		public override string ToString() {
+			return string.Format("({0}) {1} {2} = {3} ({4})", isField ? "field" : "property", type, name, Value, ArgumentType);
+		}
+	}
+}
