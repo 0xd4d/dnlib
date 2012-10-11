91a02b222703e1a61e67e710d890d22602cd149f
de4dot <de4dot@gmail.com>
2012-10-11 07:30:24 +0200

    Add CustomAttributeCollection class

---
+++ src/DotNet/CustomAttributeCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace dot10.DotNet {
+	/// <summary>
+	/// Stores <see cref="CustomAttribute"/>s
+	/// </summary>
+	public class CustomAttributeCollection : LazyList<CustomAttribute> {
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		internal CustomAttributeCollection() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="length">Initial length of the list</param>
+		/// <param name="context">Context passed to <paramref name="readOriginalValue"/></param>
+		/// <param name="readOriginalValue">Delegate instance that returns original values</param>
+		internal CustomAttributeCollection(int length, object context, MFunc<object, uint, CustomAttribute> readOriginalValue)
+			: base(length, context, readOriginalValue) {
+		}
+	}
+}
