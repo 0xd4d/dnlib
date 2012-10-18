923b6b6be5ce283d53c9f1679a5211dafd9cf684
de4dot <de4dot@gmail.com>
2012-10-18 05:06:44 +0200

    Move ComImageFlags to its own file

---
+++ src/DotNet/MD/ComImageFlags.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace dot10.DotNet.MD {
+	/// <summary>
+	/// See COMIMAGE_FLAGS_XXX in CorHdr.h in the Windows SDK
+	/// </summary>
+	[Flags]
+	public enum ComImageFlags : uint {
+		/// <summary>
+		/// See COMIMAGE_FLAGS_ILONLY in the Windows SDK
+		/// </summary>
+		ILOnly = 1,
+
+		/// <summary>
+		/// See COMIMAGE_FLAGS_32BITREQUIRED in the Windows SDK
+		/// </summary>
+		_32BitRequired = 2,
+
+		/// <summary>
+		/// Set if a native header exists (COMIMAGE_FLAGS_IL_LIBRARY)
+		/// </summary>
+		ILLibrary = 4,
+
+		/// <summary>
+		/// See COMIMAGE_FLAGS_STRONGNAMESIGNED in the Windows SDK
+		/// </summary>
+		StrongNameSigned = 8,
+
+		/// <summary>
+		/// See COMIMAGE_FLAGS_NATIVE_ENTRYPOINT in the Windows SDK
+		/// </summary>
+		NativeEntryPoint = 0x10,
+
+		/// <summary>
+		/// See COMIMAGE_FLAGS_TRACKDEBUGDATA in the Windows SDK
+		/// </summary>
+		TrackDebugData = 0x10000,
+
+		/// <summary>
+		/// See COMIMAGE_FLAGS_32BITPREFERRED in the Windows SDK
+		/// </summary>
+		_32BitPreferred = 0x20000,
+	}
+}
