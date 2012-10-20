6dcb0d9c0fee8bedc864327f7f52d265a6c72e92
de4dot <de4dot@gmail.com>
2012-10-20 16:18:15 +0200

    Add ModuleWriterException class

---
+++ src/DotNet/Writer/ModuleWriterException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace dot10.DotNet.Writer {
+	/// <summary>
+	/// Thrown when the module writer encounters an unrecoverable error
+	/// </summary>
+	[Serializable]
+	class ModuleWriterException : Exception {
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public ModuleWriterException() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="message">Error message</param>
+		public ModuleWriterException(string message)
+			: base(message) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="message">Error message</param>
+		/// <param name="innerException">Other exception</param>
+		public ModuleWriterException(string message, Exception innerException)
+			: base(message, innerException) {
+		}
+	}
+}
