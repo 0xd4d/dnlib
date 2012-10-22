fb4b0b34d384752ddf3d8d398132aa2aefb40f92
de4dot <de4dot@gmail.com>
2012-10-22 09:09:47 +0200

    Add MethodBodyWriter class and common base class

---
+++ src/DotNet/Writer/MethodBodyWriter.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.IO;
+using dot10.DotNet.Emit;
+using dot10.DotNet.MD;
+
+namespace dot10.DotNet.Writer {
+	/// <summary>
+	/// Returns tokens of token types, strings and signatures
+	/// </summary>
+	public interface ITokenCreator {
+		/// <summary>
+		/// Gets the token of <paramref name="o"/>
+		/// </summary>
+		/// <param name="o">A token type or a string or a signature</param>
+		/// <returns>The token</returns>
+		MDToken GetToken(object o);
+
+		/// <summary>
+		/// Called when an error is detected (eg. a null pointer). The error can be
+		/// ignored but the method won't be valid.
+		/// </summary>
+		/// <param name="message">Error message</param>
+		void Error(string message);
+	}
+
+	/// <summary>
+	/// Writes CIL method bodies
+	/// </summary>
+	public sealed class MethodBodyWriter : MethodBodyWriterBase {
+		ITokenCreator helper;
+		CilBody cilBody;
+		uint codeSize;
+		uint maxStack;
+		byte[] code;
+		byte[] extraSections;
+
+		/// <summary>
+		/// Gets the code as a byte array. This is valid only after calling <see cref="Write()"/>.
+		/// The size of this array is not necessarily a multiple of 4, even if there are exception
+		/// handlers present. See also <see cref="GetFullMethodBody()"/>
+		/// </summary>
+		public byte[] Code {
+			get { return code; }
+		}
+
+		/// <summary>
+		/// Gets the extra sections (exception handlers) as a byte array or <c>null</c> if there
+		/// are no exception handlers. This is valid only after calling <see cref="Write()"/>
+		/// </summary>
+		public byte[] ExtraSections {
+			get { return extraSections; }
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="helper">Helps this instance</param>
+		/// <param name="cilBody">The CIL method body</param>
+		public MethodBodyWriter(ITokenCreator helper, CilBody cilBody)
+			: base(cilBody.Instructions, cilBody.ExceptionHandlers) {
+			this.helper = helper;
+			this.cilBody = cilBody;
+		}
+
+		/// <summary>
+		/// Writes the method body
+		/// </summary>
+		public void Write() {
+			codeSize = InitializeInstructionOffsets();
+			maxStack = GetMaxStack();
+			if (NeedFatHeader())
+				WriteFatHeader();
+			else
+				WriteTinyHeader();
+			if (exceptionHandlers.Count > 0)
+				WriteExceptionHandlers();
+		}
+
+		/// <summary>
+		/// Gets the code and (possible) exception handlers in one array. The exception handlers
+		/// are 4-byte aligned.
+		/// </summary>
+		/// <returns>The code and any exception handlers</returns>
+		public byte[] GetFullMethodBody() {
+			int padding = Utils.AlignUp(code.Length, 4) - code.Length;
+			var bytes = new byte[code.Length + padding + (extraSections == null ? 0 : extraSections.Length)];
+			Array.Copy(code, 0, bytes, 0, code.Length);
+			if (extraSections != null)
+				Array.Copy(extraSections, 0, bytes, code.Length + padding, extraSections.Length);
+			return bytes;
+		}
+
+		bool NeedFatHeader() {
+			//TODO: If locals has cust attrs, we also need a fat header
+			return codeSize > 0x3F ||
+					exceptionHandlers.Count > 0 ||
+					cilBody.HasLocals ||
+					maxStack > 8;
+		}
+
+		void WriteFatHeader() {
+			code = new byte[12 + codeSize];
+
+			if (maxStack > ushort.MaxValue) {
+				Error("MaxStack is too big");
+				maxStack = ushort.MaxValue;
+			}
+
+			ushort flags = 0x3003;
+			if (exceptionHandlers.Count > 0)
+				flags |= 8;
+			if (cilBody.InitLocals)
+				flags |= 0x10;
+
+			uint localVarSigTok = 0;	//TODO:
+
+			var writer = new BinaryWriter(new MemoryStream(code));
+			writer.Write(flags);
+			writer.Write((ushort)maxStack);
+			writer.Write(codeSize);
+			writer.Write(localVarSigTok);
+			WriteInstructions(writer);
+		}
+
+		void WriteTinyHeader() {
+			code = new byte[1 + codeSize];
+			var writer = new BinaryWriter(new MemoryStream(code));
+			writer.Write((byte)((codeSize << 2) | 2));
+			WriteInstructions(writer);
+		}
+
+		void WriteExceptionHandlers() {
+			var outStream = new MemoryStream();
+			var writer = new BinaryWriter(outStream);
+			if (NeedFatExceptionClauses())
+				WriteFatExceptionClauses(writer);
+			else
+				WriteSmallExceptionClauses(writer);
+			extraSections = outStream.ToArray();
+		}
+
+		bool NeedFatExceptionClauses() {
+			// Size must fit in a byte, and since one small exception record is 12 bytes
+			// and header is 4 bytes: x*12+4 <= 255 ==> x <= 20
+			if (exceptionHandlers.Count > 20)
+				return true;
+
+			foreach (var eh in exceptionHandlers) {
+				if (!FitsInSmallExceptionClause(eh.TryStart, eh.TryEnd))
+					return true;
+				if (!FitsInSmallExceptionClause(eh.HandlerStart, eh.HandlerEnd))
+					return true;
+			}
+
+			return false;
+		}
+
+		bool FitsInSmallExceptionClause(Instruction start, Instruction end) {
+			uint offs1 = GetOffset2(start);
+			uint offs2 = GetOffset2(end);
+			if (offs2 < offs1)
+				return false;
+			return offs1 <= ushort.MaxValue && offs2 - offs1 <= byte.MaxValue;
+		}
+
+		uint GetOffset2(Instruction instr) {
+			if (instr == null)
+				return codeSize;
+			return GetOffset(instr);
+		}
+
+		void WriteFatExceptionClauses(BinaryWriter writer) {
+			const int maxExceptionHandlers = (0x00FFFFFF - 4) / 24;
+			int numExceptionHandlers = exceptionHandlers.Count;
+			if (numExceptionHandlers > maxExceptionHandlers) {
+				Error("Too many exception handlers");
+				numExceptionHandlers = maxExceptionHandlers;
+			}
+
+			writer.Write((((uint)numExceptionHandlers * 24 + 4) << 8) | 0x41);
+			for (int i = 0; i < numExceptionHandlers; i++) {
+				var eh = exceptionHandlers[i];
+				uint offs1, offs2;
+
+				offs1 = GetOffset2(eh.TryStart);
+				offs2 = GetOffset2(eh.TryEnd);
+				if (offs2 <= offs1)
+					Error("Exception handler: TryEnd <= TryStart");
+				writer.Write(offs1);
+				writer.Write(offs2 - offs1);
+
+				offs1 = GetOffset2(eh.HandlerStart);
+				offs2 = GetOffset2(eh.HandlerEnd);
+				if (offs2 <= offs1)
+					Error("Exception handler: HandlerEnd <= HandlerStart");
+				writer.Write(offs1);
+				writer.Write(offs2 - offs1);
+
+				if (eh.HandlerType == ExceptionClause.Catch)
+					writer.Write(helper.GetToken(eh.CatchType).Raw);
+				else if (eh.HandlerType == ExceptionClause.Filter)
+					writer.Write(GetOffset2(eh.FilterStart));
+				else
+					writer.Write(0);
+			}
+		}
+
+		void WriteSmallExceptionClauses(BinaryWriter writer) {
+			const int maxExceptionHandlers = (0xFF - 4) / 12;
+			int numExceptionHandlers = exceptionHandlers.Count;
+			if (numExceptionHandlers > maxExceptionHandlers) {
+				Error("Too many exception handlers");
+				numExceptionHandlers = maxExceptionHandlers;
+			}
+
+			writer.Write((((uint)numExceptionHandlers * 12 + 4) << 8) | 1);
+			for (int i = 0; i < numExceptionHandlers; i++) {
+				var eh = exceptionHandlers[i];
+				uint offs1, offs2;
+
+				offs1 = GetOffset2(eh.TryStart);
+				offs2 = GetOffset2(eh.TryEnd);
+				if (offs2 <= offs1)
+					Error("Exception handler: TryEnd <= TryStart");
+				writer.Write((ushort)offs1);
+				writer.Write((byte)(offs2 - offs1));
+
+				offs1 = GetOffset2(eh.HandlerStart);
+				offs2 = GetOffset2(eh.HandlerEnd);
+				if (offs2 <= offs1)
+					Error("Exception handler: HandlerEnd <= HandlerStart");
+				writer.Write((ushort)offs1);
+				writer.Write((byte)(offs2 - offs1));
+
+				if (eh.HandlerType == ExceptionClause.Catch)
+					writer.Write(helper.GetToken(eh.CatchType).Raw);
+				else if (eh.HandlerType == ExceptionClause.Filter)
+					writer.Write(GetOffset2(eh.FilterStart));
+				else
+					writer.Write(0);
+			}
+		}
+
+		/// <inheritdoc/>
+		protected override void ErrorImpl(string message) {
+			helper.Error(message);
+		}
+
+		/// <inheritdoc/>
+		protected override void WriteInlineField(BinaryWriter writer, Instruction instr) {
+			writer.Write(helper.GetToken(instr.Operand).Raw);
+		}
+
+		/// <inheritdoc/>
+		protected override void WriteInlineMethod(BinaryWriter writer, Instruction instr) {
+			writer.Write(helper.GetToken(instr.Operand).Raw);
+		}
+
+		/// <inheritdoc/>
+		protected override void WriteInlineSig(BinaryWriter writer, Instruction instr) {
+			writer.Write(helper.GetToken(instr.Operand).Raw);
+		}
+
+		/// <inheritdoc/>
+		protected override void WriteInlineString(BinaryWriter writer, Instruction instr) {
+			writer.Write(helper.GetToken(instr.Operand).Raw);
+		}
+
+		/// <inheritdoc/>
+		protected override void WriteInlineTok(BinaryWriter writer, Instruction instr) {
+			writer.Write(helper.GetToken(instr.Operand).Raw);
+		}
+
+		/// <inheritdoc/>
+		protected override void WriteInlineType(BinaryWriter writer, Instruction instr) {
+			writer.Write(helper.GetToken(instr.Operand).Raw);
+		}
+	}
+}
---
+++ src/DotNet/Writer/MethodBodyWriterBase.cs
@@ -0,0 +1,409 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using dot10.DotNet.Emit;
+
+namespace dot10.DotNet.Writer {
+	/// <summary>
+	/// Base class of all CIL method body writers
+	/// </summary>
+	public abstract class MethodBodyWriterBase {
+		/// <summary/>
+		protected IList<Instruction> instructions;
+		/// <summary/>
+		protected IList<ExceptionHandler> exceptionHandlers;
+		Dictionary<Instruction, uint> offsets = new Dictionary<Instruction, uint>();
+		uint firstInstructionOffset;
+		int errors;
+
+		/// <summary>
+		/// <c>true</c> if there was at least one error
+		/// </summary>
+		public bool ErrorDetected {
+			get { return errors > 0; }
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="instructions">All instructions</param>
+		/// <param name="exceptionHandlers">All exception handlers</param>
+		protected MethodBodyWriterBase(IList<Instruction> instructions, IList<ExceptionHandler> exceptionHandlers) {
+			this.instructions = instructions;
+			this.exceptionHandlers = exceptionHandlers;
+		}
+
+		/// <summary>
+		/// Called when an error is detected (eg. a null pointer). The error can be
+		/// ignored but the method won't be valid.
+		/// </summary>
+		/// <param name="message">Error message</param>
+		protected void Error(string message) {
+			errors++;
+			ErrorImpl(message);
+		}
+
+		/// <summary>
+		/// Called when an error is detected (eg. a null pointer). The error can be
+		/// ignored but the method won't be valid.
+		/// </summary>
+		/// <param name="message">Error message</param>
+		protected virtual void ErrorImpl(string message) {
+		}
+
+		/// <summary>
+		/// Gets max stack value
+		/// </summary>
+		protected uint GetMaxStack() {
+			uint maxStack;
+			if (!MaxStackCalculator.GetMaxStack(instructions, exceptionHandlers, out maxStack)) {
+				Error("Error calculating max stack value");
+				maxStack += 8;
+			}
+			return maxStack;
+		}
+
+		/// <summary>
+		/// Gets the offset of an instruction
+		/// </summary>
+		/// <param name="instr">The instruction</param>
+		/// <returns>The offset or <c>0</c> if <paramref name="instr"/> is <c>null</c> or not
+		/// present in the list of all instructions.</returns>
+		protected uint GetOffset(Instruction instr) {
+			if (instr == null) {
+				Error("Instruction is null");
+				return 0;
+			}
+			uint offset;
+			if (offsets.TryGetValue(instr, out offset))
+				return offset;
+			Error("Found some other method's instruction or a removed instruction");
+			return 0;
+		}
+
+		/// <summary>
+		/// Initializes instruction offsets and returns the total code size
+		/// </summary>
+		/// <returns>Size of code</returns>
+		protected uint InitializeInstructionOffsets() {
+			uint offset = 0;
+			foreach (var instr in instructions) {
+				if (instr == null)
+					continue;
+				offsets[instr] = offset;
+				offset += GetSizeOfInstruction(instr);
+			}
+			return offset;
+		}
+
+		/// <summary>
+		/// Gets the size of an instruction
+		/// </summary>
+		/// <param name="instr">The instruction</param>
+		/// <returns>Size of the instruction in bytes</returns>
+		protected virtual uint GetSizeOfInstruction(Instruction instr) {
+			return (uint)instr.GetSize();
+		}
+
+		/// <summary>
+		/// Writes all instructions to <paramref name="writer"/> at its current offset
+		/// </summary>
+		/// <param name="writer">The instruction writer</param>
+		/// <returns>Number of bytes written</returns>
+		protected uint WriteInstructions(BinaryWriter writer) {
+			firstInstructionOffset = (uint)writer.BaseStream.Position;
+			foreach (var instr in instructions) {
+				if (instr == null)
+					continue;
+				WriteInstruction(writer, instr);
+			}
+			return ToInstructionOffset(writer) - firstInstructionOffset;
+		}
+
+		/// <summary>
+		/// Gets the current offset in the instruction stream. This offset is relative to
+		/// the first written instruction.
+		/// </summary>
+		/// <param name="writer">The instruction writer</param>
+		/// <returns>Current offset, relative to the first written instruction</returns>
+		protected uint ToInstructionOffset(BinaryWriter writer) {
+			return (uint)writer.BaseStream.Position - firstInstructionOffset;
+		}
+
+		/// <summary>
+		/// Writes an instruction
+		/// </summary>
+		/// <param name="writer">The instruction writer</param>
+		/// <param name="instr">The instruction</param>
+		protected virtual void WriteInstruction(BinaryWriter writer, Instruction instr) {
+			WriteOpCode(writer, instr);
+			WriteOperand(writer, instr);
+		}
+
+		/// <summary>
+		/// Writes an instruction's opcode
+		/// </summary>
+		/// <param name="writer">The instruction writer</param>
+		/// <param name="instr">The instruction</param>
+		protected virtual void WriteOpCode(BinaryWriter writer, Instruction instr) {
+			var code = instr.OpCode.Code;
+			if ((ushort)code <= 0xFF)
+				writer.Write((byte)code);
+			else if (((ushort)code >> 8) == 0xFE) {
+				writer.Write((byte)((ushort)code >> 8));
+				writer.Write((byte)code);
+			}
+			else if (code == Code.UNKNOWN1)
+				writer.Write((byte)0);
+			else if (code == Code.UNKNOWN2)
+				writer.Write((ushort)0);
+			else {
+				Error("Unknown instruction");
+				writer.Write((byte)0);
+			}
+		}
+
+		/// <summary>
+		/// Writes an instruction's operand
+		/// </summary>
+		/// <param name="writer">The instruction writer</param>
+		/// <param name="instr">The instruction</param>
+		protected virtual void WriteOperand(BinaryWriter writer, Instruction instr) {
+			switch (instr.OpCode.OperandType) {
+			case OperandType.InlineBrTarget:	WriteInlineBrTarget(writer, instr); break;
+			case OperandType.InlineField:		WriteInlineField(writer, instr); break;
+			case OperandType.InlineI:			WriteInlineI(writer, instr); break;
+			case OperandType.InlineI8:			WriteInlineI8(writer, instr); break;
+			case OperandType.InlineMethod:		WriteInlineMethod(writer, instr); break;
+			case OperandType.InlineNone:		WriteInlineNone(writer, instr); break;
+			case OperandType.InlinePhi:			WriteInlinePhi(writer, instr); break;
+			case OperandType.InlineR:			WriteInlineR(writer, instr); break;
+			case OperandType.InlineSig:			WriteInlineSig(writer, instr); break;
+			case OperandType.InlineString:		WriteInlineString(writer, instr); break;
+			case OperandType.InlineSwitch:		WriteInlineSwitch(writer, instr); break;
+			case OperandType.InlineTok:			WriteInlineTok(writer, instr); break;
+			case OperandType.InlineType:		WriteInlineType(writer, instr); break;
+			case OperandType.InlineVar:			WriteInlineVar(writer, instr); break;
+			case OperandType.ShortInlineBrTarget: WriteShortInlineBrTarget(writer, instr); break;
+			case OperandType.ShortInlineI:		WriteShortInlineI(writer, instr); break;
+			case OperandType.ShortInlineR:		WriteShortInlineR(writer, instr); break;
+			case OperandType.ShortInlineVar:	WriteShortInlineVar(writer, instr); break;
+
+			default:
+				Error("Unknown operand type");
+				break;
+			}
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineBrTarget"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlineBrTarget(BinaryWriter writer, Instruction instr) {
+			uint displ = GetOffset(instr.Operand as Instruction) - (ToInstructionOffset(writer) + 4);
+			writer.Write(displ);
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineField"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected abstract void WriteInlineField(BinaryWriter writer, Instruction instr);
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineI"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlineI(BinaryWriter writer, Instruction instr) {
+			if (instr.Operand is int)
+				writer.Write((int)instr.Operand);
+			else {
+				Error("Operand is not an Int32");
+				writer.Write(0);
+			}
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineI8"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlineI8(BinaryWriter writer, Instruction instr) {
+			if (instr.Operand is long)
+				writer.Write((long)instr.Operand);
+			else {
+				Error("Operand is not an Int64");
+				writer.Write(0L);
+			}
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineMethod"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected abstract void WriteInlineMethod(BinaryWriter writer, Instruction instr);
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineNone"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlineNone(BinaryWriter writer, Instruction instr) {
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlinePhi"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlinePhi(BinaryWriter writer, Instruction instr) {
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineR"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlineR(BinaryWriter writer, Instruction instr) {
+			if (instr.Operand is double)
+				writer.Write((double)instr.Operand);
+			else {
+				Error("Operand is not a Double");
+				writer.Write(0D);
+			}
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineSig"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected abstract void WriteInlineSig(BinaryWriter writer, Instruction instr);
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineString"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected abstract void WriteInlineString(BinaryWriter writer, Instruction instr);
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineSwitch"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlineSwitch(BinaryWriter writer, Instruction instr) {
+			var targets = instr.Operand as IList<Instruction>;
+			if (targets == null) {
+				Error("switch operand is not a list of instructions");
+				writer.Write(0);
+			}
+			else {
+				uint offsetAfter = (uint)(ToInstructionOffset(writer) + 4 + targets.Count * 4);
+				writer.Write(targets.Count);
+				foreach (var target in targets)
+					writer.Write(GetOffset(target) - offsetAfter);
+			}
+		}
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineTok"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected abstract void WriteInlineTok(BinaryWriter writer, Instruction instr);
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineType"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected abstract void WriteInlineType(BinaryWriter writer, Instruction instr);
+
+		/// <summary>
+		/// Writes an <see cref="OperandType.InlineVar"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteInlineVar(BinaryWriter writer, Instruction instr) {
+			var variable = instr.Operand as IVariable;
+			if (variable == null) {
+				Error("Operand is not a local/arg");
+				writer.Write((ushort)0);
+			}
+			else if (ushort.MinValue <= variable.Number && variable.Number <= ushort.MaxValue)
+				writer.Write((ushort)variable.Number);
+			else {
+				Error("Local/arg index doesn't fit in a UInt16");
+				writer.Write((ushort)0);
+			}
+		}
+
+		/// <summary>
+		/// Writes a <see cref="OperandType.ShortInlineBrTarget"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteShortInlineBrTarget(BinaryWriter writer, Instruction instr) {
+			int displ = (int)(GetOffset(instr.Operand as Instruction) - (ToInstructionOffset(writer) + 1));
+			if (sbyte.MinValue <= displ && displ <= sbyte.MaxValue)
+				writer.Write((sbyte)displ);
+			else {
+				Error("Target instruction is too far away for a short branch");
+				writer.Write((byte)0);
+			}
+		}
+
+		/// <summary>
+		/// Writes a <see cref="OperandType.ShortInlineI"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteShortInlineI(BinaryWriter writer, Instruction instr) {
+			if (instr.Operand is sbyte)
+				writer.Write((sbyte)instr.Operand);
+			else if (instr.Operand is byte)
+				writer.Write((byte)instr.Operand);
+			else {
+				Error("Operand is not a Byte or a SByte");
+				writer.Write((byte)0);
+			}
+		}
+
+		/// <summary>
+		/// Writes a <see cref="OperandType.ShortInlineR"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteShortInlineR(BinaryWriter writer, Instruction instr) {
+			if (instr.Operand is float)
+				writer.Write((float)instr.Operand);
+			else {
+				Error("Operand is not a Single");
+				writer.Write(0F);
+			}
+		}
+
+		/// <summary>
+		/// Writes a <see cref="OperandType.ShortInlineVar"/> operand
+		/// </summary>
+		/// <param name="writer">Instruction writer</param>
+		/// <param name="instr">Instruction</param>
+		protected virtual void WriteShortInlineVar(BinaryWriter writer, Instruction instr) {
+			var variable = instr.Operand as IVariable;
+			if (variable == null) {
+				Error("Operand is not a local/arg");
+				writer.Write((byte)0);
+			}
+			else if (byte.MinValue <= variable.Number && variable.Number <= byte.MaxValue)
+				writer.Write((byte)variable.Number);
+			else {
+				Error("Local/arg index doesn't fit in a Byte");
+				writer.Write((byte)0);
+			}
+		}
+	}
+}
