11c49d1f1e1ae21b6f2f4343c4981174c13db33c
de4dot <de4dot@gmail.com>
2012-10-24 19:50:57 +0200

    Move MDStreamFlags to its own file

---
+++ src/DotNet/MD/MDStreamFlags.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dot10.DotNet.MD {
+	/// <summary>
+	/// MDStream flags
+	/// </summary>
+	[Flags]
+	public enum MDStreamFlags : byte {
+		/// <summary>#Strings stream is big and requires 4 byte offsets</summary>
+		BigStrings = 1,
+		/// <summary>#GUID stream is big and requires 4 byte offsets</summary>
+		BigGUID = 2,
+		/// <summary>#Blob stream is big and requires 4 byte offsets</summary>
+		BigBlob = 4,
+		/// <summary/>
+		Padding = 8,
+		/// <summary/>
+		DeltaOnly = 0x20,
+		/// <summary>Extra data follows the row counts</summary>
+		ExtraData = 0x40,
+		/// <summary>Set if certain tables can contain deleted rows. The name column (if present) is set to "_Deleted"</summary>
+		HasDelete = 0x80,
+	}
+}
--- src/DotNet/MD/TablesStream.cs
+++ src/DotNet/MD/TablesStream.cs
@@ -6,27 +6,6 @@ using dot10.PE;
 
 namespace dot10.DotNet.MD {
 	/// <summary>
-	/// MDStream flags
-	/// </summary>
-	[Flags]
-	public enum MDStreamFlags : byte {
-		/// <summary>#Strings stream is big and requires 4 byte offsets</summary>
-		BigStrings = 1,
-		/// <summary>#GUID stream is big and requires 4 byte offsets</summary>
-		BigGUID = 2,
-		/// <summary>#Blob stream is big and requires 4 byte offsets</summary>
-		BigBlob = 4,
-		/// <summary/>
-		Padding = 8,
-		/// <summary/>
-		DeltaOnly = 0x20,
-		/// <summary>Extra data follows the row counts</summary>
-		ExtraData = 0x40,
-		/// <summary>Set if certain tables can contain deleted rows. The name column (if present) is set to "_Deleted"</summary>
-		HasDelete = 0x80,
-	}
-
-	/// <summary>
 	/// .NET metadata tables stream
 	/// </summary>
 	public sealed partial class TablesStream : DotNetStream {
