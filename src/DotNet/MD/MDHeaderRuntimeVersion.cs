4f1386af89d7690c67f9b8f4dc41d70cac9f9b48
de4dot <de4dot@gmail.com>
2012-10-27 00:01:16 +0200

    Move MDHeaderRuntimeVersion to its own file

---
+++ src/DotNet/MD/MDHeaderRuntimeVersion.cs
@@ -0,0 +1,36 @@
+﻿namespace dot10.DotNet.MD {
+	/// <summary>
+	/// Version strings found in the meta data header
+	/// </summary>
+	public static class MDHeaderRuntimeVersion {
+		/// <summary>
+		/// MS CLR 1.0 version string (.NET 1.0)
+		/// </summary>
+		public const string MS_CLR_10 = "v1.0.3705";
+
+		/// <summary>
+		/// MS CLR 1.1 version string (.NET 1.1)
+		/// </summary>
+		public const string MS_CLR_11 = "v1.1.4322";
+
+		/// <summary>
+		/// MS CLR 2.0 version string (.NET 2.0-3.5)
+		/// </summary>
+		public const string MS_CLR_20 = "v2.0.50727";
+
+		/// <summary>
+		/// MS CLR 4.0 version string (.NET 4.0-4.5)
+		/// </summary>
+		public const string MS_CLR_40 = "v4.0.30319";
+
+		/// <summary>
+		/// ECMA 2002 version string
+		/// </summary>
+		public const string ECMA_2002 = "Standard CLI 2002";
+
+		/// <summary>
+		/// ECMA 2005 version string
+		/// </summary>
+		public const string ECMA_2005 = "Standard CLI 2005";
+	}
+}
--- src/DotNet/Writer/MetaDataHeader.cs
+++ src/DotNet/Writer/MetaDataHeader.cs
@@ -11,39 +11,9 @@ namespace dot10.DotNet.Writer {
 	/// </summary>
 	public sealed class MetaDataHeaderOptions {
 		/// <summary>
-		/// MS CLR 1.0 version string (.NET 1.0)
-		/// </summary>
-		public const string MS_CLR_10 = "v1.0.3705";
-
-		/// <summary>
-		/// MS CLR 1.1 version string (.NET 1.1)
-		/// </summary>
-		public const string MS_CLR_11 = "v1.1.4322";
-
-		/// <summary>
-		/// MS CLR 2.0 version string (.NET 2.0-3.5)
-		/// </summary>
-		public const string MS_CLR_20 = "v2.0.50727";
-
-		/// <summary>
-		/// MS CLR 4.0 version string (.NET 4.0-4.5)
-		/// </summary>
-		public const string MS_CLR_40 = "v4.0.30319";
-
-		/// <summary>
-		/// ECMA 2002 version string
-		/// </summary>
-		public const string ECMA_2002 = "Standard CLI 2002";
-
-		/// <summary>
-		/// ECMA 2005 version string
-		/// </summary>
-		public const string ECMA_2005 = "Standard CLI 2005";
-
-		/// <summary>
 		/// Default version string
 		/// </summary>
-		public const string DEFAULT_VERSION_STRING = MS_CLR_20;
+		public const string DEFAULT_VERSION_STRING = MDHeaderRuntimeVersion.MS_CLR_20;
 
 		/// <summary>
 		/// Default header signature
