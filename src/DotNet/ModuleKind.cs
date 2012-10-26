553f5d1b792ac2e839bcffa7f8f852bf0144cfc3
de4dot <de4dot@gmail.com>
2012-10-27 00:00:15 +0200

    Add ModuleKind enum

---
+++ src/DotNet/ModuleKind.cs
@@ -0,0 +1,26 @@
+﻿namespace dot10.DotNet {
+	/// <summary>
+	/// Module kind
+	/// </summary>
+	public enum ModuleKind {
+		/// <summary>
+		/// Console UI module
+		/// </summary>
+		Console,
+
+		/// <summary>
+		/// Windows GUI module
+		/// </summary>
+		Windows,
+
+		/// <summary>
+		/// DLL module
+		/// </summary>
+		Dll,
+
+		/// <summary>
+		/// Netmodule (it has no assembly manifest)
+		/// </summary>
+		Netmodule,
+	}
+}
