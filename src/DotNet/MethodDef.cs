b55a5c1852d7644535851c855cb407dcaf25789d
de4dot <de4dot@gmail.com>
2012-11-04 12:47:56 +0100

    Make DeclaringType2 public

--- src/DotNet/MethodDef.cs
+++ src/DotNet/MethodDef.cs
@@ -139,9 +139,11 @@ namespace dot10.DotNet {
 		}
 
 		/// <summary>
-		/// Called by <see cref="DeclaringType"/>
+		/// Called by <see cref="DeclaringType"/> and should normally not be called by any user
+		/// code. Use <see cref="DeclaringType"/> instead. Only call this if you must set the
+		/// declaring type without inserting it in the declaring type's method list.
 		/// </summary>
-		protected internal abstract TypeDef DeclaringType2 { get; set; }
+		public abstract TypeDef DeclaringType2 { get; set; }
 
 		/// <summary>
 		/// Gets/sets the CIL method body
@@ -760,7 +762,7 @@ namespace dot10.DotNet {
 		}
 
 		/// <inheritdoc/>
-		protected internal override TypeDef DeclaringType2 {
+		public override TypeDef DeclaringType2 {
 			get { return declaringType; }
 			set {
 				declaringType = value;
@@ -1002,7 +1004,7 @@ namespace dot10.DotNet {
 		}
 
 		/// <inheritdoc/>
-		protected internal override TypeDef DeclaringType2 {
+		public override TypeDef DeclaringType2 {
 			get { return declaringType.Value; }
 			set {
 				declaringType.Value = value;
