7b3e2e1f97d00dcdd1016a9629032b455703201f
de4dot <de4dot@gmail.com>
2012-12-05 21:14:23 +0100

    Use the original ImageBase if possible

--- src/DotNet/Writer/ModuleWriterBase.cs
+++ src/DotNet/Writer/ModuleWriterBase.cs
@@ -191,6 +191,7 @@ namespace dot10.DotNet.Writer {
 				this.PEHeadersOptions.TimeDateStamp = peImage.ImageNTHeaders.FileHeader.TimeDateStamp;
 				this.PEHeadersOptions.MajorLinkerVersion = peImage.ImageNTHeaders.OptionalHeader.MajorLinkerVersion;
 				this.PEHeadersOptions.MinorLinkerVersion = peImage.ImageNTHeaders.OptionalHeader.MinorLinkerVersion;
+				this.PEHeadersOptions.ImageBase = modDefMD.MetaData.PEImage.ImageNTHeaders.OptionalHeader.ImageBase;
 				this.AddCheckSum = modDefMD.MetaData.PEImage.ImageNTHeaders.OptionalHeader.CheckSum != 0;
 			}
 
