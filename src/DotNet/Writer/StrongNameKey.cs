f962e1678cf76b21afbcadf469af7ea99c70fac0
de4dot <de4dot@gmail.com>
2012-12-19 01:12:49 +0100

    Add StrongNamePublicKey, CreateCounterSignature()
    - Add class StrongNamePublicKey
    - Rename InvalidStrongNameKeyException -> InvalidKeyException
    - Add enum SignatureAlgorithm
    - Move some helper methods to a new static class StrongNameUtils
    - Remove StrongNameKey.bitLength
    - Add StrongNameKey properties to get/set all fields
    - Move StrongNameKey.CreatePublicKey() body to StrongNamePublicKey
    - Add StrongNameKey.CreateStrongName()
    - Add StrongNameKey static methods: CreateCounterSignature() and CreateCounterSignatureAsString()

--- src/DotNet/Writer/StrongNameKey.cs
+++ src/DotNet/Writer/StrongNameKey.cs
@@ -4,21 +4,21 @@ using System.Security.Cryptography;
 
 namespace dot10.DotNet.Writer {
 	/// <summary>
-	/// Thrown if the strong name key is invalid
+	/// Thrown if the strong name key or public key is invalid
 	/// </summary>
 	[Serializable]
-	public class InvalidStrongNameKeyException : Exception {
+	public class InvalidKeyException : Exception {
 		/// <summary>
 		/// Default constructor
 		/// </summary>
-		public InvalidStrongNameKeyException() {
+		public InvalidKeyException() {
 		}
 
 		/// <summary>
 		/// Constructor
 		/// </summary>
 		/// <param name="message">Error message</param>
-		public InvalidStrongNameKeyException(string message)
+		public InvalidKeyException(string message)
 			: base(message) {
 		}
 
@@ -27,8 +27,225 @@ namespace dot10.DotNet.Writer {
 		/// </summary>
 		/// <param name="message">Error message</param>
 		/// <param name="innerException">Other exception</param>
-		public InvalidStrongNameKeyException(string message, Exception innerException)
+		public InvalidKeyException(string message, Exception innerException)
 			: base(message, innerException) {
+		}
+	}
+
+	/// <summary>
+	/// Type of signature algorithm. See WinCrypt.h in the Windows SDK
+	/// </summary>
+	public enum SignatureAlgorithm : uint {
+		/// <summary>
+		/// RSA signature algorithm
+		/// </summary>
+		CALG_RSA_SIGN = 0x00002400,
+	}
+
+	static class StrongNameUtils {
+		public static byte[] ReadBytesReverse(this BinaryReader reader, int len) {
+			var data = reader.ReadBytes(len);
+			if (data.Length != len)
+				throw new InvalidKeyException("Can't read more bytes");
+			Array.Reverse(data);
+			return data;
+		}
+
+		public static void WriteReverse(this BinaryWriter writer, byte[] data) {
+			var d = (byte[])data.Clone();
+			Array.Reverse(d);
+			writer.Write(d);
+		}
+	}
+
+	/// <summary>
+	/// A public key
+	/// </summary>
+	public sealed class StrongNamePublicKey {
+		const uint RSA1_SIG = 0x31415352;
+		SignatureAlgorithm signatureAlgorithm;
+		AssemblyHashAlgorithm hashAlgorithm;
+		byte[] modulus;
+		byte[] publicExponent;
+
+		/// <summary>
+		/// Gets/sets the signature algorithm
+		/// </summary>
+		public SignatureAlgorithm SignatureAlgorithm {
+			get { return signatureAlgorithm; }
+			set { signatureAlgorithm = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the hash algorithm
+		/// </summary>
+		public AssemblyHashAlgorithm HashAlgorithm {
+			get { return hashAlgorithm; }
+			set { hashAlgorithm = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the modulus
+		/// </summary>
+		public byte[] Modulus {
+			get { return modulus; }
+			set { modulus = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the public exponent
+		/// </summary>
+		public byte[] PublicExponent {
+			get { return publicExponent; }
+			set {
+				if (publicExponent == null || publicExponent.Length != 4)
+					throw new ArgumentException("PublicExponent must be exactly 4 bytes");
+				publicExponent = value;
+			}
+		}
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public StrongNamePublicKey() {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="modulus">Modulus</param>
+		/// <param name="publicExponent">Public exponent</param>
+		public StrongNamePublicKey(byte[] modulus, byte[] publicExponent)
+			: this(modulus, publicExponent, AssemblyHashAlgorithm.SHA1, SignatureAlgorithm.CALG_RSA_SIGN) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="modulus">Modulus</param>
+		/// <param name="publicExponent">Public exponent</param>
+		/// <param name="hashAlgorithm">Hash algorithm</param>
+		public StrongNamePublicKey(byte[] modulus, byte[] publicExponent, AssemblyHashAlgorithm hashAlgorithm)
+			: this(modulus, publicExponent, hashAlgorithm, SignatureAlgorithm.CALG_RSA_SIGN) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="modulus">Modulus</param>
+		/// <param name="publicExponent">Public exponent</param>
+		/// <param name="hashAlgorithm">Hash algorithm</param>
+		/// <param name="signatureAlgorithm">Signature algorithm</param>
+		public StrongNamePublicKey(byte[] modulus, byte[] publicExponent, AssemblyHashAlgorithm hashAlgorithm, SignatureAlgorithm signatureAlgorithm) {
+			this.signatureAlgorithm = signatureAlgorithm;
+			this.hashAlgorithm = hashAlgorithm;
+			this.modulus = modulus;
+			this.publicExponent = publicExponent;
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="pk">Public key</param>
+		public StrongNamePublicKey(PublicKey pk)
+			: this(pk.Data) {
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="pk">Public key data</param>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
+		public StrongNamePublicKey(byte[] pk) {
+			Initialize(new BinaryReader(new MemoryStream(pk)));
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="filename">Public key file</param>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
+		public StrongNamePublicKey(string filename) {
+			using (var fileStream = File.OpenRead(filename))
+				Initialize(new BinaryReader(fileStream));
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="stream">Public key stream</param>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
+		public StrongNamePublicKey(Stream stream) {
+			Initialize(new BinaryReader(stream));
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="reader">Public key reader</param>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
+		public StrongNamePublicKey(BinaryReader reader) {
+			Initialize(reader);
+		}
+
+		void Initialize(BinaryReader reader) {
+			try {
+				// Read PublicKeyBlob
+				signatureAlgorithm = (SignatureAlgorithm)reader.ReadUInt32();
+				hashAlgorithm = (AssemblyHashAlgorithm)reader.ReadUInt32();
+				int pkLen = reader.ReadInt32();
+
+				// Read PUBLICKEYSTRUC
+				if (reader.ReadByte() != 6)
+					throw new InvalidKeyException("Not a public key");
+				if (reader.ReadByte() != 2)
+					throw new InvalidKeyException("Invalid version");
+				reader.ReadUInt16();	// reserved
+				if ((SignatureAlgorithm)reader.ReadUInt32() != SignatureAlgorithm.CALG_RSA_SIGN)
+					throw new InvalidKeyException("Not RSA sign");
+
+				// Read RSAPUBKEY
+				if (reader.ReadUInt32() != RSA1_SIG)	// magic = RSA1
+					throw new InvalidKeyException("Invalid RSA1 magic");
+				uint bitLength = reader.ReadUInt32();
+				publicExponent = reader.ReadBytesReverse(4);
+
+				modulus = reader.ReadBytesReverse((int)(bitLength / 8));
+			}
+			catch (IOException ex) {
+				throw new InvalidKeyException("Invalid public key", ex);
+			}
+		}
+
+		/// <summary>
+		/// Creates a public key blob
+		/// </summary>
+		public byte[] CreatePublicKey() {
+			return CreatePublicKey(signatureAlgorithm, hashAlgorithm, modulus, publicExponent);
+		}
+
+		internal static byte[] CreatePublicKey(SignatureAlgorithm sigAlg, AssemblyHashAlgorithm hashAlg, byte[] modulus, byte[] publicExponent) {
+			if (sigAlg != SignatureAlgorithm.CALG_RSA_SIGN)
+				throw new ArgumentException("Signature algorithm must be RSA");
+			var outStream = new MemoryStream();
+			var writer = new BinaryWriter(outStream);
+			writer.Write((uint)sigAlg);		// SigAlgID
+			writer.Write((uint)hashAlg);	// HashAlgID
+			writer.Write(0x14 + modulus.Length);// cbPublicKey
+			writer.Write((byte)6);			// bType (public key)
+			writer.Write((byte)2);			// bVersion
+			writer.Write((ushort)0);		// reserved
+			writer.Write((uint)sigAlg);		// aiKeyAlg
+			writer.Write(RSA1_SIG);			// magic (RSA1)
+			writer.Write(modulus.Length * 8);	// bitlen
+			writer.WriteReverse(publicExponent);// pubexp
+			writer.WriteReverse(modulus);	// modulus
+			return outStream.ToArray();
+		}
+
+		/// <inheritdoc/>
+		public override string ToString() {
+			return Utils.ToHex(CreatePublicKey(), false);
 		}
 	}
 
@@ -36,10 +253,9 @@ namespace dot10.DotNet.Writer {
 	/// Stores a strong name key pair
 	/// </summary>
 	public sealed class StrongNameKey {
-		const uint CALG_RSA_SIGN = 0x00002400;
+		const uint RSA2_SIG = 0x32415352;
 		byte[] publicKey;
 		AssemblyHashAlgorithm hashAlg;
-		uint bitLength;
 		byte[] publicExponent;
 		byte[] modulus;
 		byte[] prime1;
@@ -77,6 +293,74 @@ namespace dot10.DotNet.Writer {
 		}
 
 		/// <summary>
+		/// Gets/sets the public exponent
+		/// </summary>
+		public byte[] PublicExponent {
+			get { return publicExponent; }
+			set {
+				if (publicExponent == null || publicExponent.Length != 4)
+					throw new ArgumentException("PublicExponent must be exactly 4 bytes");
+				publicExponent = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets/sets the modulus
+		/// </summary>
+		public byte[] Modulus {
+			get { return modulus; }
+			set { modulus = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets prime1
+		/// </summary>
+		public byte[] Prime1 {
+			get { return prime1; }
+			set { prime1 = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets prime2
+		/// </summary>
+		public byte[] Prime2 {
+			get { return prime2; }
+			set { prime2 = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets exponent1
+		/// </summary>
+		public byte[] Exponent1 {
+			get { return exponent1; }
+			set { exponent1 = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets exponent2
+		/// </summary>
+		public byte[] Exponent2 {
+			get { return exponent2; }
+			set { exponent2 = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the coefficient
+		/// </summary>
+		public byte[] Coefficient {
+			get { return coefficient; }
+			set { coefficient = value; }
+		}
+
+		/// <summary>
+		/// Gets/sets the private exponent
+		/// </summary>
+		public byte[] PrivateExponent {
+			get { return privateExponent; }
+			set { privateExponent = value; }
+		}
+
+		/// <summary>
 		/// Default constructor
 		/// </summary>
 		public StrongNameKey() {
@@ -86,7 +370,7 @@ namespace dot10.DotNet.Writer {
 		/// Constructor
 		/// </summary>
 		/// <param name="keyData">Strong name key data</param>
-		/// <exception cref="InvalidStrongNameKeyException">Strong name key is invalid</exception>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
 		public StrongNameKey(byte[] keyData) {
 			Initialize(new BinaryReader(new MemoryStream(keyData)));
 		}
@@ -95,7 +379,7 @@ namespace dot10.DotNet.Writer {
 		/// Constructor
 		/// </summary>
 		/// <param name="filename">Strong name key file</param>
-		/// <exception cref="InvalidStrongNameKeyException">Strong name key is invalid</exception>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
 		public StrongNameKey(string filename) {
 			using (var fileStream = File.OpenRead(filename))
 				Initialize(new BinaryReader(fileStream));
@@ -105,7 +389,7 @@ namespace dot10.DotNet.Writer {
 		/// Constructor
 		/// </summary>
 		/// <param name="stream">Strong name key stream</param>
-		/// <exception cref="InvalidStrongNameKeyException">Strong name key is invalid</exception>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
 		public StrongNameKey(Stream stream) {
 			Initialize(new BinaryReader(stream));
 		}
@@ -114,7 +398,7 @@ namespace dot10.DotNet.Writer {
 		/// Constructor
 		/// </summary>
 		/// <param name="reader">Strong name key reader</param>
-		/// <exception cref="InvalidStrongNameKeyException">Strong name key is invalid</exception>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
 		public StrongNameKey(BinaryReader reader) {
 			Initialize(reader);
 		}
@@ -123,7 +407,7 @@ namespace dot10.DotNet.Writer {
 		/// Initializes the public/private key pair data
 		/// </summary>
 		/// <param name="reader">Public/private key pair reader</param>
-		/// <exception cref="InvalidStrongNameKeyException">Strong name key is invalid</exception>
+		/// <exception cref="InvalidKeyException">Strong name key is invalid</exception>
 		public void Initialize(BinaryReader reader) {
 			/*
 			 * Links:
@@ -173,67 +457,39 @@ namespace dot10.DotNet.Writer {
 
 				// Read PUBLICKEYSTRUC
 				if (reader.ReadByte() != 7)
-					throw new InvalidStrongNameKeyException("Not a public/private key pair");
+					throw new InvalidKeyException("Not a public/private key pair");
 				if (reader.ReadByte() != 2)
-					throw new InvalidStrongNameKeyException("Invalid version");
+					throw new InvalidKeyException("Invalid version");
 				reader.ReadUInt16();	// reserved
-				if (reader.ReadUInt32() != CALG_RSA_SIGN)
-					throw new InvalidStrongNameKeyException("Not RSA sign");
+				if ((SignatureAlgorithm)reader.ReadUInt32() != SignatureAlgorithm.CALG_RSA_SIGN)
+					throw new InvalidKeyException("Not RSA sign");
 
 				// Read RSAPUBKEY
-				if (reader.ReadUInt32() != 0x32415352)	// magic = RSA2
-					throw new InvalidStrongNameKeyException("Invalid RSA2 magic");
-				bitLength = reader.ReadUInt32();
-				publicExponent = ReadData(reader, 4);
+				if (reader.ReadUInt32() != RSA2_SIG)	// magic = RSA2
+					throw new InvalidKeyException("Invalid RSA2 magic");
+				uint bitLength = reader.ReadUInt32();
+				publicExponent = reader.ReadBytesReverse(4);
 
 				int len8 = (int)(bitLength / 8);
 				int len16 = (int)(bitLength / 16);
 
 				// Read the rest
-				modulus = ReadData(reader, len8);
-				prime1 = ReadData(reader, len16);
-				prime2 = ReadData(reader, len16);
-				exponent1 = ReadData(reader, len16);
-				exponent2 = ReadData(reader, len16);
-				coefficient = ReadData(reader, len16);
-				privateExponent = ReadData(reader, len8);
+				modulus = reader.ReadBytesReverse(len8);
+				prime1 = reader.ReadBytesReverse(len16);
+				prime2 = reader.ReadBytesReverse(len16);
+				exponent1 = reader.ReadBytesReverse(len16);
+				exponent2 = reader.ReadBytesReverse(len16);
+				coefficient = reader.ReadBytesReverse(len16);
+				privateExponent = reader.ReadBytesReverse(len8);
 			}
 			catch (IOException ex) {
-				throw new InvalidStrongNameKeyException("Couldn't read strong name key", ex);
+				throw new InvalidKeyException("Couldn't read strong name key", ex);
 			}
-		}
-
-		static byte[] ReadData(BinaryReader reader, int len) {
-			var data = reader.ReadBytes(len);
-			if (data.Length != len)
-				throw new InvalidStrongNameKeyException("Can't read more bytes");
-			Array.Reverse(data);
-			return data;
 		}
 
 		byte[] CreatePublicKey() {
-			uint halg = (uint)(hashAlg == 0 ? AssemblyHashAlgorithm.SHA1 : hashAlg);
-
-			var outStream = new MemoryStream();
-			var writer = new BinaryWriter(outStream);
-			writer.Write(CALG_RSA_SIGN);	// SigAlgID
-			writer.Write(halg);				// HashAlgID
-			writer.Write(0x14 + modulus.Length);	// cbPublicKey
-			writer.Write((byte)6);			// bType (public key)
-			writer.Write((byte)2);			// bVersion
-			writer.Write((ushort)0);		// reserved
-			writer.Write(CALG_RSA_SIGN);	// aiKeyAlg
-			writer.Write(0x31415352);		// magic (RSA1)
-			writer.Write(bitLength);		// bitlen
-			WriteReverse(writer, publicExponent);	// pubexp
-			WriteReverse(writer, modulus);	// modulus
-			return outStream.ToArray();
-		}
-
-		static void WriteReverse(BinaryWriter writer, byte[] data) {
-			var d = (byte[])data.Clone();
-			Array.Reverse(d);
-			writer.Write(d);
+			var halg = hashAlg == 0 ? AssemblyHashAlgorithm.SHA1 : hashAlg;
+			return StrongNamePublicKey.CreatePublicKey(SignatureAlgorithm.CALG_RSA_SIGN, halg, modulus, publicExponent);
 		}
 
 		/// <summary>
@@ -258,6 +514,64 @@ namespace dot10.DotNet.Writer {
 			catch {
 				((IDisposable)rsa).Dispose();
 				throw;
+			}
+		}
+
+		/// <summary>
+		/// Creates a strong name blob
+		/// </summary>
+		public byte[] CreateStrongName() {
+			var outStream = new MemoryStream();
+			var writer = new BinaryWriter(outStream);
+			writer.Write((byte)7);			// bType (public/private key)
+			writer.Write((byte)2);			// bVersion
+			writer.Write((ushort)0);		// reserved
+			writer.Write((uint)SignatureAlgorithm.CALG_RSA_SIGN);	// aiKeyAlg
+			writer.Write(RSA2_SIG);			// magic (RSA1)
+			writer.Write(modulus.Length * 8);	// bitlen
+			writer.WriteReverse(publicExponent);
+			writer.WriteReverse(modulus);
+			writer.WriteReverse(prime1);
+			writer.WriteReverse(prime2);
+			writer.WriteReverse(exponent1);
+			writer.WriteReverse(exponent2);
+			writer.WriteReverse(coefficient);
+			writer.WriteReverse(privateExponent);
+			return outStream.ToArray();
+		}
+
+		/// <summary>
+		/// Creates a counter signature, just like
+		/// <c>sn -a IdentityPubKey.snk IdentityKey.snk SignaturePubKey.snk</c> can do.
+		/// The public key <c>sn</c> prints is <paramref name="signaturePubKey"/>'s value.
+		/// </summary>
+		/// <param name="identityPubKey">Identity public key</param>
+		/// <param name="identityKey">Identity strong name key pair</param>
+		/// <param name="signaturePubKey">Signature public key</param>
+		/// <returns>The counter signature as a hex string</returns>
+		public static string CreateCounterSignatureAsString(StrongNamePublicKey identityPubKey, StrongNameKey identityKey, StrongNamePublicKey signaturePubKey) {
+			var counterSignature = CreateCounterSignature(identityPubKey, identityKey, signaturePubKey);
+			return Utils.ToHex(counterSignature, false);
+		}
+
+		/// <summary>
+		/// Creates a counter signature, just like
+		/// <c>sn -a IdentityPubKey.snk IdentityKey.snk SignaturePubKey.snk</c> can do.
+		/// The public key <c>sn</c> prints is <paramref name="signaturePubKey"/>'s value.
+		/// </summary>
+		/// <param name="identityPubKey">Identity public key</param>
+		/// <param name="identityKey">Identity strong name key pair</param>
+		/// <param name="signaturePubKey">Signature public key</param>
+		/// <returns>The counter signature</returns>
+		public static byte[] CreateCounterSignature(StrongNamePublicKey identityPubKey, StrongNameKey identityKey, StrongNamePublicKey signaturePubKey) {
+			var hash = AssemblyHash.Hash(signaturePubKey.CreatePublicKey(), identityPubKey.HashAlgorithm);
+			using (var rsa = identityKey.CreateRSA()) {
+				var rsaFmt = new RSAPKCS1SignatureFormatter(rsa);
+				string hashName = identityPubKey.HashAlgorithm.GetName();
+				rsaFmt.SetHashAlgorithm(hashName);
+				var snSig = rsaFmt.CreateSignature(hash);
+				Array.Reverse(snSig);
+				return snSig;
 			}
 		}
 	}
