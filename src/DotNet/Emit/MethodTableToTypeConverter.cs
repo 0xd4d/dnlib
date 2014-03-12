08655ca03676622189088545abd649876df6340a
de4dot <de4dot@gmail.com>
2014-03-12 05:36:19 +0100

    It can now convert any .NET 2.0 type addr to System.Type

--- src/DotNet/Emit/MethodTableToTypeConverter.cs
+++ src/DotNet/Emit/MethodTableToTypeConverter.cs
@@ -33,9 +33,6 @@ namespace dnlib.DotNet.Emit {
 	/// Converts a type address to a type. The address can be found in RuntimeTypeHandle.Value
 	/// and it's the same address you use with the WinDbg SOS command !dumpmt.
 	/// </summary>
-	/// <remarks>It currently doesn't support all possible type addresses if it's .NET 2.0.
-	/// Only addresses of normal types can then be used, eg. <c>SomeType</c> but not
-	/// <c>SomeType*</c> or <c>SomeType[]</c>, etc.</remarks>
 	static class MethodTableToTypeConverter {
 		const string METHOD_NAME = "m";
 		static readonly MethodInfo setMethodBodyMethodInfo = typeof(MethodBuilder).GetMethod("SetMethodBody", BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
@@ -43,20 +40,20 @@ namespace dnlib.DotNet.Emit {
 		static readonly FieldInfo sigDoneFieldInfo = typeof(SignatureHelper).GetField("m_sigDone", BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 		static readonly FieldInfo currSigFieldInfo = typeof(SignatureHelper).GetField("m_currSig", BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 		static readonly FieldInfo signatureFieldInfo = typeof(SignatureHelper).GetField("m_signature", BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+		static readonly FieldInfo ptrFieldInfo = typeof(RuntimeTypeHandle).GetField("m_ptr", BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
 		static Dictionary<IntPtr, Type> addrToType = new Dictionary<IntPtr, Type>();
-		static Dictionary<object, bool> alreadyInitialized;
 		static ModuleBuilder moduleBuilder;
 		static int numNewTypes;
 
 		static MethodTableToTypeConverter() {
-			var asmb = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("DynAsm"), AssemblyBuilderAccess.Run);
-			moduleBuilder = asmb.DefineDynamicModule("DynMod");
+			if (ptrFieldInfo == null) {
+				var asmb = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("DynAsm"), AssemblyBuilderAccess.Run);
+				moduleBuilder = asmb.DefineDynamicModule("DynMod");
+			}
 		}
 
 		/// <summary>
-		/// Converts <paramref name="address"/> to a <see cref="Type"/>. Only addresses of simple
-		/// types (eg. <c>SomeType</c> but not <c>SomeType*</c> or <c>SomeType[]</c>, etc) are
-		/// supported if .NET 2.0 is used.
+		/// Converts <paramref name="address"/> to a <see cref="Type"/>.
 		/// </summary>
 		/// <param name="address">Address of type</param>
 		/// <returns>The <see cref="Type"/> or <c>null</c></returns>
@@ -65,59 +62,9 @@ namespace dnlib.DotNet.Emit {
 			if (addrToType.TryGetValue(address, out type))
 				return type;
 
-			type = GetTypeUsingTypeBuilder(address);
-			if (type == null) {
-				// Always call this method since additional assemblies could've been loaded
-				InitializeAllSimpleTypeAddresses();
-				addrToType.TryGetValue(address, out type);
-			}
-
+			type = GetTypeNET20(address) ?? GetTypeUsingTypeBuilder(address);
 			addrToType[address] = type;
-
 			return type;
-		}
-
-		static void InitializeAllSimpleTypeAddresses() {
-			if (alreadyInitialized == null)
-				alreadyInitialized = new Dictionary<object, bool>();
-			foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
-				foreach (var mod in GetModules(asm)) {
-					if (alreadyInitialized.ContainsKey(mod))
-						continue;
-
-					// The reason mod.GetTypes() isn't used is that if it's an obfuscated
-					// assembly with invalid types or if it's a ModuleBuilder with a type
-					// that hasn't been finished yet, an exception is thrown.
-					for (int i = 2; ; i++) {
-						Type type;
-						try {
-							type = mod.ResolveType(0x02000000 + i);
-						}
-						catch (TypeLoadException) {
-							// Invalid type or a type that hasn't been finished building yet.
-							continue;
-						}
-						catch (ArgumentException) {
-							// No more types left
-							break;
-						}
-						addrToType[type.TypeHandle.Value] = type;
-					}
-
-					var modType = mod.GetType();
-					if (modType != typeof(ModuleBuilder) && modType.ToString() != "System.Reflection.Emit.InternalModuleBuilder")
-						alreadyInitialized[mod] = true;
-				}
-			}
-		}
-
-		static IEnumerable<Module> GetModules(Assembly asm) {
-			try {
-				return asm.GetModules();
-			}
-			catch {
-			}
-			return asm.GetLoadedModules();
 		}
 
 		static Type GetTypeUsingTypeBuilder(IntPtr address) {
@@ -131,7 +78,7 @@ namespace dnlib.DotNet.Emit {
 				if (setMethodBodyMethodInfo != null)
 					return GetTypeNET45(tb, mb, address);
 				else
-					return GetTypeNET20(tb, mb, address);
+					return GetTypeNET40(tb, mb, address);
 			}
 			catch {
 				moduleBuilder = null;
@@ -150,9 +97,9 @@ namespace dnlib.DotNet.Emit {
 			return createdMethod.GetMethodBody().LocalVariables[0].LocalType;
 		}
 
-		// .NET 2.0 - 4.5. This code works with .NET 4.0+ but will throw an exception
-		// if .NET 2.0 is used ("operation could destabilize the runtime")
-		static Type GetTypeNET20(TypeBuilder tb, MethodBuilder mb, IntPtr address) {
+		// This code works with .NET 4.0+ but will throw an exception if .NET 2.0 is used
+		// ("operation could destabilize the runtime")
+		static Type GetTypeNET40(TypeBuilder tb, MethodBuilder mb, IntPtr address) {
 			var ilg = mb.GetILGenerator();
 			ilg.Emit(SR.Emit.OpCodes.Ret);
 
@@ -167,6 +114,15 @@ namespace dnlib.DotNet.Emit {
 
 			var createdMethod = tb.CreateType().GetMethod(METHOD_NAME, BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
 			return createdMethod.GetMethodBody().LocalVariables[0].LocalType;
+		}
+
+		// .NET 2.0 - 3.5
+		static Type GetTypeNET20(IntPtr address) {
+			if (ptrFieldInfo == null)
+				return null;
+			object th = new RuntimeTypeHandle();
+			ptrFieldInfo.SetValue(th, address);
+			return Type.GetTypeFromHandle((RuntimeTypeHandle)th);
 		}
 
 		static string GetNextTypeName() {
