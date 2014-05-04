d97cca76bf0669354243bf5786cc04c86ae134fc
de4dot <de4dot@gmail.com>
2014-05-04 23:05:58 +0200

    Remove recursive calls from AllTypesHelper

--- src/DotNet/AllTypesHelper.cs
+++ src/DotNet/AllTypesHelper.cs
@@ -29,34 +29,28 @@ namespace dnlib.DotNet {
 	/// Returns types without getting stuck in an infinite loop
 	/// </summary>
 	public struct AllTypesHelper {
-		Dictionary<TypeDef, bool> visited;
-		RecursionCounter recursionCounter;
-
 		/// <summary>
 		/// Gets a list of all types and nested types
 		/// </summary>
 		/// <param name="types">A list of types</param>
 		public static IEnumerable<TypeDef> Types(IEnumerable<TypeDef> types) {
-			var helper = new AllTypesHelper();
-			helper.visited = new Dictionary<TypeDef, bool>();
-			return helper.GetTypes(types);
-		}
-
-		IEnumerable<TypeDef> GetTypes(IEnumerable<TypeDef> types) {
-			if (!recursionCounter.Increment()) {
-			}
-			else {
-				foreach (var type in types.GetSafeEnumerable()) {
+			var visited = new Dictionary<TypeDef, bool>();
+			var stack = new Stack<IEnumerator<TypeDef>>();
+			if (types != null)
+				stack.Push(types.GetSafeEnumerable().GetEnumerator());
+			while (stack.Count > 0) {
+				var enumerator = stack.Pop();
+				while (enumerator.MoveNext()) {
+					var type = enumerator.Current;
 					if (visited.ContainsKey(type))
 						continue;
 					visited[type] = true;
 					yield return type;
 					if (type.NestedTypes.Count > 0) {
-						foreach (var nested in GetTypes(type.NestedTypes))
-							yield return nested;
+						stack.Push(enumerator);
+						enumerator = type.NestedTypes.GetSafeEnumerable().GetEnumerator();
 					}
 				}
-				recursionCounter.Decrement();
 			}
 		}
 	}
