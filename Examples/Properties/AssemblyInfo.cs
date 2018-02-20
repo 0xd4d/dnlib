0cdb282a252058b48c41f3c6855daf6de31041a5
SlowLogicBoy <SlowLogicBoy@users.noreply.github.com>
2018-02-20 13:56:22 +0200

    Revert .sln and Example projects

---
+++ Examples/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dnlib.Examples")]
+[assembly: AssemblyDescription("dnlib examples")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dnlib.Examples")]
+[assembly: AssemblyCopyright("Copyright (C) 2012-2014 de4dot@gmail.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
