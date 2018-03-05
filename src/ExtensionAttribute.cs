1748c52f9355002087e309118a322b5df6e46102
de4dot <de4dot@gmail.com>
2018-03-05 18:26:26 +0100

    Target .NET Framework 3.5, Client Profile

--- src/ExtensionAttribute.cs
+++
@@ -1,9 +0,0 @@
-// dnlib: See LICENSE.txt for more info
-
-#pragma warning disable 1591	// XML doc warning
-
-namespace System.Runtime.CompilerServices {
-	[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
-	public sealed class ExtensionAttribute : Attribute {
-	}
-}
