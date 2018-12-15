4eff4c5e9f093fef80005120bedd0e1d6e951823
de4dot <de4dot@gmail.com>
2018-12-15 09:14:36 +0100

    Verify length

--- src/DotNet/Pdb/Managed/DbiFunction.cs
+++ src/DotNet/Pdb/Managed/DbiFunction.cs
@@ -68,7 +68,7 @@ namespace dnlib.DotNet.Pdb.Managed {
 		public int AsyncKickoffMethod {
 			get {
 				var data = Root.GetSymAttribute(asyncMethodInfoAttributeName);
-				if (data == null)
+				if (data == null || data.Length < 4)
 					return 0;
 				return BitConverter.ToInt32(data, 0);
 			}
@@ -77,7 +77,7 @@ namespace dnlib.DotNet.Pdb.Managed {
 		public uint? AsyncCatchHandlerILOffset {
 			get {
 				var data = Root.GetSymAttribute(asyncMethodInfoAttributeName);
-				if (data == null)
+				if (data == null || data.Length < 8)
 					return null;
 				uint token = BitConverter.ToUInt32(data, 4);
 				return token == uint.MaxValue ? (uint?)null : token;
@@ -95,7 +95,7 @@ namespace dnlib.DotNet.Pdb.Managed {
 
 		SymbolAsyncStepInfo[] CreateSymbolAsyncStepInfos() {
 			var data = Root.GetSymAttribute(asyncMethodInfoAttributeName);
-			if (data == null)
+			if (data == null || data.Length < 12)
 				return Array2.Empty<SymbolAsyncStepInfo>();
 			int pos = 8;
 			int count = BitConverter.ToInt32(data, pos);
