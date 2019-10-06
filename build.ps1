c3d660c67315cadd8f23529cb662fe073d792e59
0xd4d <de4dot@gmail.com>
2019-10-06 21:01:11 +0200

    Update build files

--- build.ps1
+++ build.ps1
@@ -1,10 +1,41 @@
+param([switch]$NoMsbuild)
+
 $ErrorActionPreference = 'Stop'
+
+$configuration = 'Release'
 
 #
 # dotnet build isn't used because it can't build net35 tfms
 #
 
-msbuild -v:m -restore -t:Build -p:Configuration=Release dnlib.sln
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-msbuild -v:m -t:Pack -p:Configuration=Release dnlib.sln
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+$env:NoTargetFrameworkNet35 = ''
+
+if ($IsWindows) {
+	$useMsbuild = $true
+}
+else {
+	$useMsbuild = $false
+}
+if ($NoMsbuild) {
+	$useMsbuild = $false
+}
+
+if (!$useMsbuild) {
+	# There are currently no net35 reference assemblies on nuget
+	$env:NoTargetFrameworkNet35 = 'true'
+}
+
+if ($useMsbuild) {
+	msbuild -v:m -restore -t:Build -p:Configuration=$configuration
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+	msbuild -v:m -t:Pack -p:Configuration=$configuration
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+else {
+	dotnet build -v:m -c $configuration
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+	dotnet pack -v:m -c $configuration src/dnlib.csproj
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+
+$env:NoTargetFrameworkNet35 = ''
